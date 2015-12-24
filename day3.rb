01f3944970d0b85d52c5df659ca7a64d97ae703c
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-12-23 20:49:47 -0800

    Solved day3 part 2 + refactor

--- day3.rb
+++ day3.rb
@@ -19,33 +19,102 @@
 # to the house at his starting/ending location.  ^v^v^v^v^v delivers a bunch
 # of presents to some very lucky children at only 2 houses.
 
+# --- Part Two ---
+
+# The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+# Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+# This year, how many houses receive at least one present?
+
+# For example:
+
+# ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+# ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+# ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
+class Santa
+  attr_accessor :row, :column
+
+  def initialize
+    @row = 0
+    @column = 0
+  end
+
+  def coordinates
+    [@row, @column]
+  end
+
+  def move_north
+    @row -= 1
+  end
+
+  def move_south
+    @row += 1
+  end
+
+  def move_west
+    @column -= 1
+  end
+
+  def move_east
+    @column += 1
+  end
+
+  def move(char)
+    case char
+    when ">"
+      move_east
+    when "^"
+      move_north
+    when "<"
+      move_west
+    when "v"
+      move_south
+    end
+  end
+end
+
 class Mapper
-  attr_accessor :visited, :row, :column
+  attr_accessor :visited, :santa
 
   def initialize
     @visited = Hash.new do |h, row|
       h[row] = Hash.new false # column => visited
     end
-    @row = 0
-    @column = 0
+    @santa = Santa.new
   end
 
   def count_houses(input)
     input.each_char.reduce 1 do |count, char|
-      @visited[@row][@column] = true
+      @visited[santa.row][santa.column] = true
+      santa.move(char)
 
-      case char
-      when ">"
-        @column += 1
-      when "^"
-        @row -= 1
-      when "<"
-        @column -= 1
-      when "v"
-        @row += 1
+      if @visited[santa.row][santa.column] == false
+        count + 1
+      else
+        count
       end
+    end
+  end
+end
 
-      if @visited[@row][@column] == false
+class RoboMapper < Mapper
+  attr_accessor :robosanta
+
+  def initialize
+    @robosanta = Santa.new
+    super
+  end
+
+  def count_houses(input)
+    input.each_char.with_index.reduce 1 do |count, (char, i)|
+      santa = i.even? ? @santa : @robosanta
+
+      @visited[santa.row][santa.column] = true
+      santa.move(char)
+
+      if @visited[santa.row][santa.column] == false
         count + 1
       else
         count
@@ -58,4 +127,6 @@ if $0 == __FILE__
   input = File.read('day3_input.txt')
   mapper = Mapper.new
   p mapper.count_houses(input)
+  rmapper = RoboMapper.new
+  p rmapper.count_houses(input)
 end
--- day3_spec.rb
+++ day3_spec.rb
@@ -1,5 +1,37 @@
 require_relative 'day3'
 
+describe Santa do
+  let(:santa) { Santa.new }
+
+  describe "coordinates" do
+    it "starts at [0, 0]" do
+      expect(santa.coordinates).to eq [0, 0]
+    end
+  end
+
+  describe "movement" do
+    it "moves north" do
+      santa.move_north
+      expect(santa.coordinates).to eq [-1, 0]
+    end
+
+    it "moves south" do
+      santa.move_south
+      expect(santa.coordinates).to eq [1, 0]
+    end
+
+    it "moves west" do
+      santa.move_west
+      expect(santa.coordinates).to eq [0, -1]
+    end
+
+    it "moves east" do
+      santa.move_east
+      expect(santa.coordinates).to eq [0, 1]
+    end
+  end
+end
+
 describe Mapper do
   let(:mapper) { Mapper.new }
   describe "count_houses" do
@@ -14,6 +46,24 @@ describe Mapper do
     it "counts houses" do
       count = mapper.count_houses("^v^v^v^v^v")
       expect(count).to eq 2
+    end
+  end
+end
+
+describe RoboMapper do
+  let(:mapper) { RoboMapper.new }
+  describe "count_houses" do
+    it "counts houses" do
+      count = mapper.count_houses("^v")
+      expect(count).to eq 3
+    end
+    it "counts houses" do
+      count = mapper.count_houses("^>v<")
+      expect(count).to eq 3
+    end
+    it "counts houses" do
+      count = mapper.count_houses("^v^v^v^v^v")
+      expect(count).to eq 11
     end
   end
 end
