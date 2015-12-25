55fda42fbd5b32fcb406eead15ecc0387e2c0352
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-12-24 22:40:14 -0500

    Solved Day 4 part 1

--- day4.rb
+++ day4.rb
@@ -16,3 +16,28 @@
 # such number to do so.  If your secret key is pqrstuv, the lowest number it
 # combines with to make an MD5 hash starting with five zeroes is 1048970; that
 # is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+
+require 'digest'
+
+class Stuffer
+  attr_accessor :digest
+
+  def initialize
+    @digest = Digest::MD5.new
+  end
+
+  def hash_key(key)
+    value = 0
+    md5 = digest.hexdigest(key + value.to_s)
+    while md5[0,5] != "00000"
+      value +=1
+      md5 = digest.hexdigest(key + value.to_s)
+    end
+    value
+  end
+end
+
+if $0 == __FILE__
+  s = Stuffer.new
+  p s.hash_key("yzbqklnj")
+end
--- day4_spec.rb
+++ day4_spec.rb
@@ -1,2 +1,13 @@
 require_relative "day4"
 
+describe Stuffer do
+  let(:s) { Stuffer.new }
+  describe "hash_key" do
+    it "returns lowest number whose md5 hash starts with five 0's" do
+      key = "abcdef"
+      expect(s.hash_key(key)).to eq 609043
+      key2 = "pqrstuv"
+      expect(s.hash_key(key2)).to eq 1048970
+    end
+  end
+end
