6bd7e079f67bb66be1033eda8618302c6ac6aadd
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-12-29 17:39:59 -0500

    Solved day 6 part 1

--- day6.rb
+++ day6.rb
@@ -7,7 +7,7 @@
 # Furthermore, because you've been especially nice this year, Santa has mailed
 # you instructions on how to display the ideal lighting configuration.
 
-# Lights in your grid are numbered from 0 to 999 in each direction; the lights
+# Display in your grid are numbered from 0 to 999 in each direction; the lights
 # at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions
 # include whether to turn on, turn off, or toggle various inclusive ranges
 # given as coordinate pairs. Each coordinate pair represents opposite corners
@@ -24,3 +24,74 @@
 # the ones that were on, and turning on the ones that were off.  turn off
 # 499,499 through 500,500 would turn off (or leave off) the middle four
 # lights.  After following the instructions, how many lights are lit?
+#
+
+class Display
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(1000) { Array.new 1000, " "}
+  end
+
+  def count_lights
+    @grid.reduce(0) do |count, row|
+      row.count("*") + count
+    end
+  end
+
+  def flip(coord1, coord2, action)
+    range(coord1, coord2) do |light|
+      case action
+      when :on
+        "*"
+      when :off
+        " "
+      when :toggle
+        light == " " ? "*" : " "
+      end
+    end
+  end
+
+  def parse_instruction instr
+    /(?<action>turn on|turn off|toggle) (?<coord1>\d+,\d+) through (?<coord2>\d+,\d+)/ =~ instr
+
+    raise "Unknown instruction #{instr}" unless action
+
+    x1, y1 = coord1.split(",").map { |c| c.to_i }
+    x2, y2 = coord2.split(",").map { |c| c.to_i }
+
+    action =
+      case action
+      when "turn on"
+        :on
+      when "turn off"
+        :off
+      when "toggle"
+        :toggle
+      end
+
+    [[x1, y1], [x2, y2], action]
+  end
+
+  private
+
+  def range(coord1, coord2)
+    x1, y1 = coord1
+    x2, y2 = coord2
+    (x1..x2).each do |row|
+      (y1..y2).each do |col|
+        grid[row][col] = yield(grid[row][col])
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  display = Display.new
+  instructions = File.readlines('day6_input.txt').map { |line| display.parse_instruction line }
+  instructions.each do |instr|
+    coord1, coord2, action = instr
+    display.flip(coord1, coord2, action)
+  end
+  p  display.count_lights
+end
--- day6_spec.rb
+++ day6_spec.rb
@@ -1,1 +1,66 @@
 require_relative 'day6'
+
+describe Display do
+  let(:lights) { Display.new }
+  describe "parse instruction" do
+    it "returns a pair of coordinates and action" do
+      result = lights.parse_instruction "turn on 887,9 through 959,629"
+
+      expect(result).to eq([[887, 9], [959, 629], :on])
+
+      result = lights.parse_instruction "turn off 887,9 through 959,629"
+
+      expect(result).to eq([[887, 9], [959, 629], :off])
+
+      result = lights.parse_instruction "toggle 887,9 through 959,629"
+
+      expect(result).to eq([[887, 9], [959, 629], :toggle])
+
+      expect { lights.parse_instruction "foo 887,9 through 959,629" }.to raise_error RuntimeError
+
+    end
+  end
+
+  describe "flip" do
+    it "turns on lights" do
+      lights.flip([0,0], [1, 1], :on)
+
+      expect(lights.grid[0][0]).to eq "*"
+      expect(lights.grid[1][0]).to eq "*"
+      expect(lights.grid[0][1]).to eq "*"
+      expect(lights.grid[1][1]).to eq "*"
+    end
+
+    it "turns off lights" do
+      lights.flip([0,0], [1, 1], :on)
+      lights.flip([0,0], [0, 1], :off)
+
+      expect(lights.grid[0][0]).to eq " "
+      expect(lights.grid[0][1]).to eq " "
+      expect(lights.grid[1][0]).to eq "*"
+      expect(lights.grid[1][1]).to eq "*"
+    end
+
+    it "toggles lights" do
+      lights.flip([0,0], [1, 1], :on)
+      lights.flip([0,0], [0, 1], :off)
+      lights.flip([0,0], [1, 1], :toggle)
+
+      expect(lights.grid[0][0]).to eq "*"
+      expect(lights.grid[0][1]).to eq "*"
+      expect(lights.grid[1][0]).to eq " "
+      expect(lights.grid[1][1]).to eq " "
+    end
+  end
+
+  describe "count_lights" do
+    it "returns number of lights lit after all instructions" do
+      lights.flip([0,0], [1, 1], :on)
+      lights.flip([0,0], [0, 1], :off)
+      lights.flip([0,0], [1, 1], :toggle)
+
+      count = lights.count_lights
+      expect(count).to eq 2
+    end
+  end
+end
