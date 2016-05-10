39fb75a401824b314d97b4b8234d6ad910488819
Hsing-Hui Hsu <hsing-hui@carezone.com>
2016-05-09 20:45:07 -0700

    Solved Day 11

---
+++ day11.rb
@@ -0,0 +1,78 @@
+# --- Day 11: Corporate Policy ---
+
+# Santa's previous password expired, and he needs help choosing a new one.
+
+# To help him remember his new password after the old one expires, Santa has
+# devised a method of coming up with a password based on the previous one.
+# Corporate policy dictates that passwords must be exactly eight lowercase
+# letters (for security reasons), so he finds his new password by incrementing
+# his old password string repeatedly until it is valid.
+
+# Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so
+# on. Increase the rightmost letter one step; if it was z, it wraps around to
+# a, and repeat with the next letter to the left until one doesn't wrap
+# around.
+
+# Unfortunately for Santa, a new Security-Elf recently started, and he has
+# imposed some additional password requirements:
+
+# - Passwords must include one increasing straight of at least three letters,
+#   like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd
+#   doesn't count.
+#
+# - Passwords may not contain the letters i, o, or l, as these
+#   letters can be mistaken for other characters and are therefore confusing.
+#
+# - Passwords must contain at least two different, non-overlapping pairs of
+#   letters, like aa, bb, or zz.
+#
+# For example:
+
+# hijklmmn meets the first requirement (because it contains the straight hij)
+# but fails the second requirement requirement (because it contains i and l).
+#
+# abbceffg meets the third requirement (because it repeats bb and ff) but
+# fails the first requirement.
+#
+# abbcegjk fails the third requirement, because
+# it only has one double letter (bb).
+#
+# The next password after abcdefgh is abcdffaa.  The next password after
+# ghijklmn is ghjaabcc, because you eventually skip all the passwords that
+# start with ghi..., since i is not allowed.
+#
+# Given Santa's current password (your puzzle input), what should
+# his next password be?
+
+# Your puzzle input is hepxcrrq.
+
+def next_password password
+  password = password.next
+  while !(valid? password)
+    password = password.next
+  end
+  password
+end
+
+def prohibited? password
+  /i|o|l/ =~ password
+end
+
+def has_double_repeated? password
+  /(.)\1.*(.)\2/ =~ password
+end
+
+def has_straight? password
+  password.chars.each_cons(3) do |a, b, c|
+    return true if a.next == b && b.next == c
+  end
+  false
+end
+
+def valid? password
+  return false if prohibited? password
+  (has_double_repeated? password) && (has_straight? password)
+end
+
+# p next_password(next_password "hepxcrrq")
+p next_password "ghijklmn"
---
+++ day11_spec.rb
@@ -0,0 +1,64 @@
+require_relative 'day11'
+
+describe 'next_password' do
+end
+
+describe "prohibited?" do
+
+  it "returns true if contains i, o, or l" do
+    expect(prohibited? "abi").to be_truthy
+    expect(prohibited? "abl").to be_truthy
+    expect(prohibited? "abo").to be_truthy
+  end
+
+  it "returns false if it does not contain i, o, or l" do
+    expect(prohibited? "abc").to be_falsey
+  end
+
+end
+
+describe "has_double_repeated?" do
+  it "returns true if there is a repeated letter" do
+    expect(has_double_repeated? "aabb").to be_truthy
+    expect(has_double_repeated? "xaaebb").to be_truthy
+  end
+
+  it "returns false if there is no repeated letter" do
+    expect(has_double_repeated? "abb").to be_falsey
+    expect(has_double_repeated? "abbg").to be_falsey
+    expect(has_double_repeated? "abcc").to be_falsey
+  end
+
+  it 'returns false for triple letters' do
+    expect(has_double_repeated? "bbb").to be_falsey
+  end
+end
+
+describe "has_straight?" do
+  it "returns true if string contains straight" do
+    expect(has_straight? "abcc").to be_truthy
+  end
+
+  it "returns false if string does not contain straight" do
+    expect(has_straight? "abdc").to be_falsey
+  end
+end
+
+describe "valid?" do
+  it "returns false for invalid password" do
+    expect(valid? "hijklmmn").to be_falsey
+    expect(valid? "abbceffg" ).to be_falsey
+    expect(valid? "abbcegjk" ).to be_falsey
+  end
+
+  it "returns true for valid passwords" do
+    expect(valid? "abcdffaa").to be_truthy
+  end
+end
+
+describe "next_password?" do
+  it "returns the next valid password" do
+    expect(next_password "abcdefgh").to eq "abcdffaa"
+    # expect(next_password "ghijklmn").to eq "ghjaabcc"
+  end
+end
