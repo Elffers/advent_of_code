876613e5088c2d9581a6fd4f49a067de20cebf00
Hsing-Hui Hsu <hsing-hui@carezone.com>
2016-05-11 00:19:32 -0700

    Parse Day 15

---
+++ day15_spec.rb
@@ -0,0 +1,39 @@
+require_relative 'day15'
+
+describe Cookie do
+  let(:input) { StringIO.new(
+    <<INPUT
+Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+INPUT
+  ) }
+
+  let(:cookie) { Cookie.new input.readlines }
+
+
+  describe 'parse' do
+    it "creates store for reindeer" do
+      cookie.parse
+
+      expected = {
+        "Butterscotch" => {
+          capacity: -1,
+          durability: -2,
+          flavor: 6,
+          texture: 3,
+          calories: 8
+        },
+
+        "Cinnamon" => {
+          capacity: 2,
+          durability: 3,
+          flavor: -2,
+          texture: -1,
+          calories: 3
+        },
+      }
+
+      expect(cookie.ingredients).to eq expected
+    end
+  end
+end
