bf5f4be8c2f469fbe35911f7a58a7d1cb5053d1f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-04 22:21:37 -0800

    Day 5 Part 2

--- 2016/day05.rb
+++ 2016/day05.rb
@@ -24,6 +24,36 @@
 # 18f47a30.
 
 # Given the actual Door ID, what is the password?
+# --- Part Two ---
+
+# As the door slides open, you are presented with a second door that uses a
+# slightly more inspired security mechanism. Clearly unimpressed by the last
+# version (in what movie is the password decrypted in order?!), the Easter
+# Bunny engineers have worked out a better solution.
+
+# Instead of simply filling in the password from left to right, the hash now
+# also indicates the position within the password to fill. You still look for
+# hashes that begin with five zeroes; however, now, the sixth character
+# represents the position (0-7), and the seventh character is the character to
+# put in that position.
+
+# A hash result of 000001f means that f is the second character in the
+# password. Use only the first result for each position, and ignore invalid
+# positions.
+
+# For example, if the Door ID is abc:
+
+# The first interesting hash is from abc3231929, which produces 0000015...;
+# so, 5 goes in position 1: _5______.  In the previous method, 5017308
+# produced an interesting hash; however, it is ignored, because it specifies
+# an invalid position (8).  The second interesting hash is at index 5357525,
+# which produces 000004e...; so, e goes in position 4: _5__e___.  You almost
+# choke on your popcorn as the final character falls into place, producing the
+# password 05ace8e3.
+
+# Given the actual Door ID and this new method, what is the password? Be extra
+# proud of your solution if it uses a cinematic "decrypting" animation.
+
 require 'digest'
 
 class PasswordGenerator
@@ -50,10 +80,38 @@ class PasswordGenerator
     end
     password
   end
+
+  def build_password2
+    password = Array.new 8
+    index = 0
+
+    while password.any? { |x| x.nil? }
+      hash = id + index.to_s
+      hex = md5.hexdigest hash
+
+      if hex.start_with? "00000"
+        position = hex[5]
+        char = hex[6]
+
+        if valid? position
+          password[position.to_i] = char if password[position.to_i].nil?
+        end
+      end
+
+      index += 1
+    end
+
+    password.join
+  end
+
+  def valid? position
+    /\d/ =~ position && position.to_i < 8
+  end
 end
 
 
 if $0 == __FILE__
   pg = PasswordGenerator.new 'uqwqemis'
   p pg.build_password
+  p pg.build_password2
 end
--- 2016/day05_spec.rb
+++ 2016/day05_spec.rb
@@ -9,4 +9,11 @@ describe PasswordGenerator do
       expect(pg.build_password).to eq expected
     end
   end
+
+  describe 'build_password2' do
+    it "builds password" do
+      expected = '05ace8e3'
+      expect(pg.build_password2).to eq expected
+    end
+  end
 end
