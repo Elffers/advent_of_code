81a4f04dd1bbacf8ee812805f21724b070204876
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-06 22:14:24 -0800

    Day7 Part 2

--- 2016/day07.rb
+++ 2016/day07.rb
@@ -20,12 +20,65 @@ def has_palin word
   (/(\w)(\w)\2\1/ =~ word) && ($1 != $2)
 end
 
-p parse 'abba[mnop]qrst'
-p parse 'abcd[bddb]xyyx'
-p parse 'aaaa[qwer]tyui'
-p parse 'ioxxoj[asdfgh]zxcvbn'
-p parse 'xdsqxnovprgovwzkus[fmadbfsbqwzzrzrgdg]aeqornszgvbizdm'
+# p parse 'abba[mnop]qrst'
+# p parse 'abcd[bddb]xyyx'
+# p parse 'aaaa[qwer]tyui'
+# p parse 'ioxxoj[asdfgh]zxcvbn'
+# p parse 'xdsqxnovprgovwzkus[fmadbfsbqwzzrzrgdg]aeqornszgvbizdm'
+
+# input = File.readlines('day07.in').map { |x| x.strip }
+# p input.count { |line| parse line }
+
+def aba? word
+  abas = []
+  i = 0
+
+  while i <= word.length - 3
+    substring = word[i..word.length]
+    if /(\w)(\w)\1/ =~ substring && $1 != $2
+      abas << $&
+    end
+    i += 1
+  end
+  abas
+end
+
+def bab? words, aba
+  a, b = aba.chars
+  bab = b + a + b
+  words.each do |word|
+    return true if word.include? bab
+  end
+  nil
+end
+
+def parse2 line
+  words = line.split(/\[|\]/)
+  abas = []
+  babs = []
+
+  words.each_with_index do |w, i|
+    if i.even?
+      if x = aba?(w)
+        abas.concat x
+      end
+    else
+      babs << w
+    end
+  end
+  # p "abas: #{abas}"
+
+  abas.each do |aba|
+    return line if bab? babs, aba
+  end
+  nil
+end
+
+
+p parse2 'aba[bab]xyz' #yes
+p parse2 'xyx[xyx]xyx' #no
+p parse2 'aaa[kek]eke' #yes
+p parse2 'zazbz[bzb]cdb' #yes
 
 input = File.readlines('day07.in').map { |x| x.strip }
-p input.count { |line| parse line }
-
+p input.count { |line| parse2 line }
