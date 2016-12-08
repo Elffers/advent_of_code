413a8770c4a63c86ef93cfd78aefcfc2a678d196
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-08 00:04:41 -0800

    Tidy up Day8

--- 2016/day08.rb
+++ 2016/day08.rb
@@ -2,22 +2,18 @@ class Auth
   attr_accessor :board
 
   def initialize row, col
-    @board = init_board row, col
+    @board = row.times.map { ['.'] * col }
   end
 
-  def init_board row, col
-    row.times.map { ['.'] * col }
-  end
-
-  def extract pixels
-    /(\d+)\D+(\d+)/ =~ pixels
+  def extract_digits instr
+    /(\d+)\D+(\d+)/ =~ instr
     [$1, $2].map(&:to_i)
   end
 
-  def follow line
-    x, y = extract line
+  def follow instr
+    x, y = extract_digits instr
 
-    if line['rect']
+    if instr['rect']
       (0...y).to_a.each do |row|
         (0...x).to_a.each do |col|
           board[row][col] = "#"
@@ -25,11 +21,11 @@ class Auth
       end
     end
 
-    if line['rotate row']
+    if instr['rotate row']
       board[x] = shift board[x], y
     end
 
-    if line['rotate column']
+    if instr['rotate column']
       t = board.transpose
       shift t[x], y
       t[x] = shift t[x], y
@@ -38,25 +34,24 @@ class Auth
   end
 
   def shift row, n
-    new_row = Array.new row.length, '.'
     length = row.length
+    new_row = ['.'] * length
 
     row.each_with_index do |x, i|
       shifted = (i + n) % length
       new_row[shifted] = x
     end
+
     new_row
   end
 
   def count_lit
     board.flatten.count { |x| x == '#' }
   end
-
 end
 
 if __FILE__ == $0
   input = File.readlines('day8.in').map{ |x| x.strip }
-
   a = Auth.new 6, 50
 
   input.each do |instr|
@@ -65,9 +60,9 @@ if __FILE__ == $0
 
   p a.count_lit
 
+  # part 2
   a.board.map(&:join).map do |line|
     line.gsub!(".", " ")
     puts line
   end
-
 end
