e2e5e2a641fe30ca7c5004e828db32ebe621cecd
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-09 00:40:47 -0800

    Clean up day9

--- 2016/day09.rb
+++ 2016/day09.rb
@@ -1,7 +1,35 @@
 require 'strscan'
 
-input = File.read('day9.in').strip
-#
+def decompressed_length input
+  s = StringScanner.new input
+
+  length = 0
+  tail_index = 0
+
+  while !s.eos?
+    if s.exist?(/\(\d+x\d+\)/)
+      s.scan_until(/\(\d+x\d+\)/)
+      prematch_length = s.pos - s.matched_size - tail_index
+
+      # count all characters up to marker
+      length += prematch_length
+
+      chars, iter = s.matched.delete("()").split('x').map(&:to_i)
+
+      # add length of decompressed sequence
+      s.pos += chars
+      length += chars * iter
+
+      # move tail up to current position
+      tail_index = s.pos
+    else
+      length += 1
+      s.pos += 1
+    end
+  end
+  length
+end
+
 # input = "ADVENT"
 # input = "A(1x5)BC"
 # input = "A(1x5)BC"
@@ -10,35 +38,6 @@ input = File.read('day9.in').strip
 # input = "(6x1)(1x3)A" #6
 # input = "X(8x2)(3x3)ABCY"
 
-s = StringScanner.new input
+input = File.read('day9.in').strip
+p decompressed_length input #112830
 
-length = 0
-tail_index = 0
-
-while !s.eos?
-  if s.exist?(/\(\d+x\d+\)/)
-    s.scan_until(/\(\d+x\d+\)/)
-    p "position after match: #{s.pos}"
-
-    prematch_length = s.pos - s.matched_size - tail_index
-
-    p "num chars before: #{prematch_length}"
-
-    length += prematch_length
-
-    chars, iter = s.matched.delete("(").delete(")").split('x').map(&:to_i)
-    p [chars, iter]
-    s.pos += chars
-    length += chars * iter
-    p "length: #{length}"
-    p "next char: #{input[s.pos]}"
-    tail_index = s.pos
-    p "---------------"
-
-  else
-    length += 1
-    s.pos += 1
-  end
-end
-
-p "length: #{length}"
