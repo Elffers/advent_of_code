ec2421f67e9f765f408c13b0391c00789170a981
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-12 23:03:51 -0800

    Solved Day 13 Part 1

--- 2016/day13.rb
+++ 2016/day13.rb
@@ -1,63 +1,55 @@
-require 'set'
-
 class Maze
-  attr_accessor :input, :queue, :y, :visited, :start, :dest
+  attr_accessor :input, :start, :dest, :visited, :queue
 
-  def initialize input, dest
+  def initialize(input, dest)
     @input = input
+    @start = [1, 1]
     @dest = dest
     @visited = Set.new
-    @start = [1, 1]
     @queue = []
   end
 
-  def open? node
-    x, y = node.x, node.y
+  def open? coords
+    x, y = coords
     a = x*x + 3*x + 2*x*y + y + y*y
-    a += 10
+    a += input
     bin = a.to_s 2
     bin.count("1").even?
   end
 
-  # def map x, y
-  #   y.times.map do |j|
-  #     x.times.map do |i|
-  #       open? i, j
-  #     end
-  #   end
-  # end
-
-  # def init_visited
-  #   x, y = dest
-  #   y.times.map do |j|
-  #     x.times.map do |i|
-  #       false
-  #     end
-  #   end
-  # end
-
   def valid? node
-    node.x >= 0 && node.y >= 0
+    x, y = node.coords
+    x >= 0 && y >= 0
   end
 
-# We LOOP till queue is not empty
-# Dequeue front cell from the queue
-# Return if the destination coordinates have reached.
-# For each of its four adjacent cells, if the value is 1 and they are not visited yet, we enqueue it in the queue and also mark them as visited.
-  def shortest_path_to dest
+  def maze
+    x, y = dest
+    y.times.map do |j|
+      x.times.map do |i|
+        open? [i, j]
+      end
+    end
+  end
 
+  def inspect_maze
+    maze.map do |x|
+      x.map do |el|
+        el ? '.' : '#'
+      end.join
+    end
+  end
+
+  def shortest_path
     current = Node.new start, 0
     queue << current
-    visited << current
-
-    dist = 0
+    visited << current.coords
 
     while !queue.empty?
       current = queue.shift
+      x, y = current.coords
 
-      return current.dist if current == dest
+      return current.dist if [x, y] == dest
 
-      x, y = current.x, current.y
       dist = current.dist + 1
 
       right = Node.new [x+1, y], dist
@@ -66,35 +58,37 @@ class Maze
       up    = Node.new [x, y-1], dist
 
       [right, left, down, up].each do |node|
-        if !visited.include?(node) && open?(node) && valid?(node)
+        if !visited.include?(node.coords) && open?(node.coords) && valid?(node)
           queue << node
-          visited << node
+          visited << node.coords
         end
-        p queue
       end
     end
-
-    dist
   end
+end
 
-  class Node
-    attr_accessor :x, :y, :dist
+class Node
+  attr_accessor :coords, :dist
 
-    def initialize coords, dist
-      @x = coords.first
-      @y = coords.last
-      @dist = dist
-    end
+  def initialize coords, dist
+    @coords = coords
+    @dist = dist
   end
-
 end
 
 
 if __FILE__ == $0
-  # dest = [31,39]
-  # m = Maze.new 1364
+
   dest = [7, 4]
   m = Maze.new 10, dest
-  p  m.shortest_path_to dest
+  p  m.shortest_path
 
+  dest = [4, 2]
+  m = Maze.new 10, dest
+  p  m.shortest_path
+
+  dest = [31, 39]
+  m = Maze.new 1364, dest
+  pp m.inspect_maze
+  p  m.shortest_path
 end
