a161f2a134553d2d69681abb571cd89acb823232
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-19 23:07:40 -0800

    Day 19 Part 2

--- 2016/day19.rb
+++ 2016/day19.rb
@@ -10,6 +10,17 @@ def find_left i, elves
   left
 end
 
+def winner elves
+  while elves.size != 1
+    half = elves.size/2
+    elves.delete elves[half]
+    elves.rotate!
+    # p elves
+  end
+  elves
+end
+
+
 if __FILE__ == $0
   count = 3001330
   # elves = count.times.map { 1 }
@@ -34,4 +45,27 @@ if __FILE__ == $0
   power = 2 ** (n - 1)
   remainder = count - power
   p remainder * 2 + 1
+
+# part 2
+
+  # elves = (1..count).to_a
+  # (1..730).each do |n|
+  #   elves = (1..n).to_a
+  #   p "#{n} #{winner elves}"
+  # end
+
+  n = 1
+  # count = 600
+  while 3 ** n <= count
+    n+=1
+  end
+  n = n - 1
+  power = 3 ** n
+  remainder = count - power
+  if remainder <power
+    p remainder
+  else
+    p power + 2*(remainder - power)
+  end
+
 end
