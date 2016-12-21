7acd501d6799a05b06b536e488768fb4a6e7c359
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-21 10:24:02 -0800

    Day 20 part 2

--- 2016/day20.rb
+++ 2016/day20.rb
@@ -12,5 +12,26 @@ ranges.each do |min, max|
     lowest = max + 1
   end
 end
-
 p lowest
+
+# part 2
+count = 0
+lowest  = 0
+highest = 0
+
+total = 4_294_967_295
+
+ranges.each do |min, max|
+  if min > highest + 1
+    lowest = min
+    whitelisted = lowest - highest - 1
+    count += whitelisted
+  end
+
+  highest = max if max > highest
+  # p [lowest, highest, count]
+end
+
+count += total - highest
+
+p count
