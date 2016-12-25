8b29e7e6400c39e5149e44859a011e342bc91218
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-12-25 13:14:06 -0500

    Day 21 Part 2, i guess

--- 2016/day21.rb
+++ 2016/day21.rb
@@ -31,57 +31,102 @@ end
 
 def rotate_pos word, char, r=false
   index = word.index(char)
-  rot = index + 1
-  rot += 1 if index >= 4
+  if r
+    # terrible hack; only works for size 8
+    map = { 1 => 1,
+            3 => 2,
+            5 => 3,
+            7 => 4,
+            2 => -2,
+            4 => -1,
+            6 => 0,
+            0 => 1 }
+    rot = map[index]
 
-  word.chars.rotate!(-rot).join
+    word.chars.rotate!(rot).join
+  else
+    rot = index + 1
+    rot += 1 if index >= 4
+
+    word.chars.rotate!(-rot).join
+  end
 end
 
 def scramble word, input, r=false
   input.each do |line|
     word =
-    if /swap\sposition\s(\d)\swith\sposition\s(\d)/ =~ line
-      x, y = $1.to_i, $2.to_i
-      swap_position word, x, y
+      if /swap\sposition\s(\d)\swith\sposition\s(\d)/ =~ line
+        x, y = $1.to_i, $2.to_i
+        swap_position word, x, y
 
-    elsif /swap\sletter\s(\w)\swith\sletter\s(\w)/ =~ line
-      x, y = $1, $2
-      swap_letters word, x, y
+      elsif /swap\sletter\s(\w)\swith\sletter\s(\w)/ =~ line
+        x, y = $1, $2
+        swap_letters word, x, y
 
-    elsif /rotate\sleft\s(\d)/ =~ line
-      steps = $1.to_i
-      rotate word, steps, right: r
+      elsif /rotate\sleft\s(\d)/ =~ line
+        steps = $1.to_i
+        rotate word, steps, right: r
 
-    elsif /rotate\sright\s(\d)/ =~ line
-      steps = $1.to_i
-      rotate word, steps, right: !r
+      elsif /rotate\sright\s(\d)/ =~ line
+        steps = $1.to_i
+        rotate word, steps, right: !r
 
-    elsif /rotate\sbased\son\sposition\sof\sletter\s(\w)/ =~ line
-      rotate_pos word, $1, r
+      elsif /rotate\sbased\son\sposition\sof\sletter\s(\w)/ =~ line
+        rotate_pos word, $1, r
 
-    elsif /reverse\spositions\s(\d)\sthrough\s(\d)/ =~ line
-      reverse(word, $1.to_i, $2.to_i)
+      elsif /reverse\spositions\s(\d)\sthrough\s(\d)/ =~ line
+        reverse(word, $1.to_i, $2.to_i)
 
-    elsif /move\sposition\s(\d)\sto\sposition\s(\d)/ =~ line
-      x, y = $1.to_i, $2.to_i
-      move_position word, x, y, r
+      elsif /move\sposition\s(\d)\sto\sposition\s(\d)/ =~ line
+        x, y = $1.to_i, $2.to_i
+        move_position word, x, y, r
 
-    end
-    p [word, line]
+      end
+    # p [word, line]
   end
 
   word
 end
 
 if __FILE__ == $0
-  test_input = File.readlines('day21_test.in').map { |x| x.strip }
-  word = 'abcde'
-  p scramble(word, test_input)
-  # this isn't right
-  p scramble('decab', test_input.reverse, true)
+  # test_input = File.readlines('day21_test.in').map { |x| x.strip }
+  # word = 'abcde'
+  # p scramble(word, test_input)
+  # p scramble('decab', test_input.reverse, true)
 
   input = File.readlines('day21.in').map { |x| x.strip }
   word = 'abcdefgh'
   scrambled = 'aefgbcdh'
-  p scramble(word, input) == scrambled
+  p scramble(word, input) == scrambled # part 1
+  p scramble(scrambled, input.reverse, true)
+
+  scrambled = 'fbgdceah'
+  p scramble(scrambled, input.reverse, true)
+
+  # b = "abcde"
+  # b.each_char.with_index do |c, i|
+  #   new_word = rotate_pos(b, c)
+  #   p [new_word, i, new_word.index(c)]
+  # end
+  # p '===='
+
+  # b = "abcdef"
+  # b.each_char.with_index do |c, i|
+  #   new_word = rotate_pos(b, c)
+  #   p [new_word, i, new_word.index(c)]
+  # end
+
+  # p '===='
+  # b = "abcdefg"
+  # b.each_char.with_index do |c, i|
+  #   new_word = rotate_pos(b, c)
+  #   p [new_word, i, new_word.index(c)]
+  # end
+
+  # p '===='
+  # b = "abcdefgh"
+  # b.each_char.with_index do |c, i|
+  #   new_word = rotate_pos(b, c)
+  #   p [new_word, c, i, new_word.index(c)]
+  # end
 end
