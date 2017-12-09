e760977df3e9c450ab9e8a5efb510cee68520c72
Hsing-Hui Hsu <hhhsu1@gmail.com>
2017-12-08 22:45:14 -0800

    Day 9 (go)

---
+++ 2017/go/day9/day09.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	// "strconv"
+)
+
+func score(input string) (score int, garbage int) {
+	depth := 0
+
+	i := 0
+	for i < len(input) {
+		char := input[i]
+		switch char {
+		case '{':
+			depth++
+		case '}':
+			score += depth
+			depth--
+		case '<':
+			i++
+			g := input[i]
+			for g != '>' {
+				if g == '!' {
+					i++
+				} else {
+					garbage++
+				}
+				i++
+				g = input[i]
+			}
+		}
+		i++
+	}
+
+	return score, garbage
+}
+
+func main() {
+	input, err := ioutil.ReadFile("day9.in")
+
+	if err == nil {
+		input := strings.TrimSpace(string(input))
+		fmt.Println(score(input))
+	}
+}
