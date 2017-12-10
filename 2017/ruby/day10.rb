36b2c41133aceed11fd4ef3d1784dfdc378b74a4
Hsing-Hui Hsu <hhhsu1@gmail.com>
2017-12-09 22:39:12 -0800

    Revise day 10 part 1

--- 2017/ruby/day10.rb
+++ 2017/ruby/day10.rb
@@ -1,36 +1,41 @@
-lengths = File.read("../day10.in").strip.split(",").map { |x| x.to_i }
-s = (0..255).to_a
+# lengths = File.read("../day10.in").strip.split(",").map { |x| x.to_i }
+# s = (0..255).to_a
 
-# lengths = [3, 4, 1, 5]
-# s = (0..4).to_a
+lengths = [3, 4, 1, 5]
+s = (0..4).to_a
 
-size = s.size
-pos = 0
-skip = 0
+def twist lengths, s
+  size = s.size
+  pos = 0
+  skip = 0
 
-while !lengths.empty?
-  len = lengths.shift
-  s = s.cycle(2).to_a
+  while !lengths.empty?
+    len = lengths.shift
+    s = s.cycle(2).to_a
 
-  sublist = s[pos, len]
-  p sublist: sublist
-  sublist.reverse!
+    sublist = s[pos, len]
+    # p sublist: sublist
+    sublist.reverse!
 
-  cdr_start = (pos + len) % size
-  cdr_end = size - len
-  cdr = s[cdr_start, cdr_end]
-  p cdr: cdr
+    cdr_start = (pos + len) % size
+    cdr_end = size - len
+    cdr = s[cdr_start, cdr_end]
+    # p cdr: cdr
 
-  s = sublist.concat cdr
-  p s_before: s
-  s = s.rotate(-pos)
-  p s: s
+    s = sublist.concat cdr
+    s = s.rotate(-pos)
+    # p s: s
 
-  pos = (pos + len + skip)%size
-  p pos: pos
-  p "==============\n"
-  skip += 1
+    pos = (pos + len + skip)%size
+    # p pos: pos
+    skip += 1
+  end
+  [s, pos, skip]
 end
 
+result = twist(lengths, s)
+p result
+s = result.first
+
 p s[0] * s[1]
-# 43890 too high
+
