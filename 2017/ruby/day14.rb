3c5c42c0985e5d52fad4b05106ec929917f013cc
Hsing-Hui Hsu <hhhsu1@gmail.com>
2017-12-13 23:57:52 -0800

    Day 14 part 2 (ruby)

--- 2017/ruby/day14.rb
+++ 2017/ruby/day14.rb
@@ -1,3 +1,6 @@
+require 'pp'
+require 'set'
+
 def knot lengths, list, pos, skip
   lens = lengths.dup
 
@@ -23,8 +26,7 @@ def knot lengths, list, pos, skip
   [list, pos, skip]
 end
 
-
-def hash input
+def knothash input
   ascii = input.chars.map { |x| x.ord }
   tail = [17, 31, 73, 47, 23]
   lengths = ascii.concat tail
@@ -45,19 +47,71 @@ end
 
 input = "hfdlxzhv"
 
-i = 0
+n = 0
 count = 0
-
+rows = []
 128.times do
-  row = "#{input}-#{i.to_s}"
-  knot_hash = hash row
+  row = "#{input}-#{n.to_s}"
+  knot_hash = knothash row
 
-  hex = knot_hash.chars.map do |x|
+  binary = knot_hash.chars.map do |x|
     format "%04b", x.to_i(16)
   end.join
 
-  count += hex.count("1")
-  i += 1
+  rows << binary
+
+  count += binary.count("1")
+  n += 1
 end
 
-p count
+p "Part 1: #{count}"
+
+def neighbors i, j
+  coords = [
+    [1, 0], [-1, 0], [0, 1], [0, -1]
+  ]
+
+  coords.map do |x, y|
+    [i + x, j + y]
+  end
+end
+
+# test = <<TEST
+# 11010100
+# 01010101
+# 00001010
+# 10101101
+# 01101000
+# 11001001
+# 01000100
+# 11010110
+# TEST
+# rows = test.split "\n"
+# pp rows: rows
+
+unseen = Set.new
+rows.each_with_index do |row, i|
+  row.chars.each_with_index do |char, j|
+    if char == "1"
+      unseen << [i, j]
+    end
+  end
+end
+
+p unseen
+
+regions = 0
+until unseen.empty?
+  queue = []
+  queue.push unseen.first
+  until queue.empty?
+    x, y = queue.shift
+    if unseen.include?([x, y])
+      unseen.delete([x, y])
+      queue.concat neighbors(x, y)
+    end
+  end
+  regions += 1
+end
+
+p "Part 2: #{regions}"
