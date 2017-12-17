d07ec31d6c19a83079fb335b0f445732aed2e0d4
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-12-16 21:48:25 -0800

    Day 17 part 2

--- 2017/ruby/day17.rb
+++ 2017/ruby/day17.rb
@@ -1,5 +1,6 @@
 steps = 349
 
+# Part 1
 buffer = [0]
 current = 0
 val = 1
@@ -10,3 +11,14 @@ while val < 2018
   val += 1
 end
 p buffer[current + 1]
+
+# Part 2
+res = nil
+(1..50_000_000).each do |size|
+  current = (current + steps) % size
+  if current == 0
+    res = size
+  end
+  current += 1
+end
+p res
