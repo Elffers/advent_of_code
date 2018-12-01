c0f9bf10895a7931f5883b33bff3363ba8824fdd
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-11-30 23:09:21 -0800

    Solved day 1

--- 2018/ruby/day01.rb
+++ 2018/ruby/day01.rb
@@ -1,2 +1,18 @@
 input = File.readlines("day01.in").map { |x| x.strip.to_i }
+# part 1
 p input.reduce(:+)
+
+# part 2
+current  = 0
+frequencies = { current => true }
+loop do
+  input.each do |x|
+    current = current + x
+    if frequencies[current]
+      p "FOUND: #{current}"
+      exit
+    else
+      frequencies[current] = true
+    end
+  end
+end
