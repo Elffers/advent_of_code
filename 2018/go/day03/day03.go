1ab97bd19080d254f8d572c72e64b09b30b196de
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-03 00:15:24 -0800

    Refactor Day3 (go)
    Weird thing where "unclaimed" is sometimes 0 but returns correct answer
    when run a second time? (not idempotent)

--- 2018/go/day03/day03.go
+++ 2018/go/day03/day03.go
@@ -8,8 +8,8 @@ import (
 
 func Populate(claims map[int]Claim) (fabric [1000][1000][]int) {
 	for id, claim := range claims {
-		for i := claim.X; i < claim.X + claim.Width; i++ {
-			for j := claim.Y; j < claim.Y + claim.Length; j++ {
+		for i := claim.X; i < claim.X+claim.Width; i++ {
+			for j := claim.Y; j < claim.Y+claim.Length; j++ {
 				fabric[j][i] = append(fabric[j][i], id)
 			}
 		}
@@ -17,41 +17,29 @@ func Populate(claims map[int]Claim) (fabric [1000][1000][]int) {
 	return
 }
 
-func Overlap(claims map[int]Claim) int {
-	overlaps := 0
+func Overlap(claims map[int]Claim) (int, int) {
+	overlaps, unclaimed := 0, 0
 	fabric := Populate(claims)
+	claimed := make(map[int]bool)
 
 	for _, row := range fabric {
 		for _, col := range row {
 			if len(col) > 1 {
 				overlaps++
-			}
-		}
-	}
-	return overlaps
-}
-
-func Unclaimed(claims map[int]Claim) int {
-	fabric := Populate(claims)
-
-	overlap := make(map[int]bool)
-	for _, row := range fabric {
-		for _, col := range row {
-			if len(col) > 1 {
-				for _, id := range col {
-					overlap[id] = true
+				for _, claim_id := range col {
+					claimed[claim_id] = true
 				}
 			}
 		}
 	}
 
 	for id, _ := range claims {
-		if _, ok := overlap[id]; !ok {
-			return id
+		if _, ok := claimed[id]; !ok {
+			unclaimed = id
 		}
 	}
 
-	return 0
+	return overlaps, unclaimed
 }
 
 func main() {
@@ -60,15 +48,15 @@ func main() {
 		panic(err)
 	}
 	claims := formatInput(string(input))
-
-	fmt.Printf("Day 3 Part 1: %+v\n", Overlap(claims))
-	fmt.Printf("Day 3 Part 2: %+v\n", Unclaimed(claims))
+	part1, part2 := Overlap(claims)
+	fmt.Printf("Day 3 Part 1: %+v\n", part1)
+	fmt.Printf("Day 3 Part 2: %+v\n", part2)
 }
 
 type Claim struct {
-	X int
-	Y int
-	Width int
+	X      int
+	Y      int
+	Width  int
 	Length int
 }
 
@@ -76,7 +64,6 @@ func formatInput(input string) map[int]Claim {
 	lines := strings.Split(input, "\n")
 	claims := map[int]Claim{}
 	for _, line := range lines {
-
 		var id, x, y, w, l int
 		// #1 @ 749,666: 27x15
 		fmt.Sscanf(line, "#%d @ %d,%d: %dx%d", &id, &x, &y, &w, &l)
