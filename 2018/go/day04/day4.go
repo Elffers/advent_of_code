a9d711382be4b8018ba6e973145214a24ce05a60
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-04 10:17:32 -0800

    Day 4 Part 2 (go)

--- 2018/go/day04/day4.go
+++ 2018/go/day04/day4.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+// Populate map with sleep schedule per guard by tallying times guard was
+// asleep in a given minute
 func GuardSchedule(logs []LogEntry) map[int]map[int]int {
 	guards := make(map[int]map[int]int)
 	guard := 0
@@ -41,15 +43,16 @@ func GuardSchedule(logs []LogEntry) map[int]map[int]int {
 
 func FindSleepiest(logs []LogEntry) int {
 	totals := make(map[int]int) // guard id : total minutes
-
 	schedule := GuardSchedule(logs)
 
+	// Accumulate total number of minutes asleep per guard
 	for id, log := range schedule {
 		for _, count := range log {
 			totals[id] += count
 		}
 	}
 
+	// Find guard who slept the most total minutes
 	var sleepiest int
 	var max int
 	for id, total := range totals {
@@ -59,26 +62,57 @@ func FindSleepiest(logs []LogEntry) int {
 		}
 	}
 
+	// Find which minute said guard was most often asleep
+	sleepiestMin, _ := sleepiestMinute(schedule[sleepiest])
+
+	return sleepiest * sleepiestMin
+}
+func sleepiestMinute(log map[int]int) (minute, count int) {
 	var sleepiestMin int
 	var maxCount int
-	for min, count := range schedule[sleepiest] {
+	for min, count := range log {
 		if count > maxCount {
 			sleepiestMin = min
 			maxCount = count
 		}
 	}
+	return sleepiestMin, maxCount
+}
 
-	return sleepiest * sleepiestMin
+func FindMostPredictablySleepy(logs []LogEntry) int {
+	mostFrequentlyAsleep := make(map[int][]int) // guard: [minute, count]
+	schedule := GuardSchedule(logs)
+
+	// Find minute each guard was most often asleep
+	for id, log := range schedule {
+		min, count := sleepiestMinute(log)
+		mostFrequentlyAsleep[id] = []int{min, count}
+	}
+	// Find which guard fell asleep most often at a given minute
+	guard, minute, count := 0, 0, 0
+	for id, minuteCount := range mostFrequentlyAsleep {
+		m := minuteCount[0]
+		c := minuteCount[1]
+		if c > count {
+			guard = id
+			minute = m
+			count = c
+		}
+	}
+
+	return guard * minute
 }
 
 func main() {
 	input, err := ioutil.ReadFile("/Users/hhh/JungleGym/advent_of_code/2018/inputs/day4.in")
+	// input, err := ioutil.ReadFile("/Users/hhh/JungleGym/advent_of_code/2018/inputs/day4test.in")
 	if err != nil {
 		panic(err)
 	}
 	logs := formatInput(string(input))
 	sort.Sort(ByTime(logs))
 	fmt.Printf("Day 4 Part 1: %+v\n", FindSleepiest(logs))
+	fmt.Printf("Day 4 Part 2: %+v\n", FindMostPredictablySleepy(logs))
 }
 
 func formatInput(input string) []LogEntry {
