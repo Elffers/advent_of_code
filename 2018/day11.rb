c438fa1b3a9125e193781759c6189442bf882935
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-11 13:05:52 -0800

    WIP day 11 cleanup
    calibrating "Part 2: 488, [238, 131, 58]"

--- 2018/day11.rb
+++ 2018/day11.rb
@@ -1,69 +1,78 @@
 require 'pp'
-serial = 3999
-n = 300
 
 def power_level(i, j, serial)
   rack_id = i + 10
-  power_level = (rack_id * j) + serial
-  power_level *= rack_id
-  r = power_level % 100
-  power_level = (power_level - r)
-  power_level = power_level/ 100
-  power_level= power_level % 10
-  power_level -= 5
+  pwr = (rack_id * j) + serial
+  pwr *= rack_id
+  pwr = (pwr/ 100) % 10 - 5
 end
 
-# grid = Hash.new { |h, k| h[k] = Hash.new }
+def grid(serial)
+  (1..300).map do |x|
+    (1..300).map do |y|
+      power_level(x, y, serial)
+    end
+  end
+end
+
+def max_square_sum(i, j, dim, grid)
+  pts = nil
+  max = 0
+
+  # find which dimension has greatest square sum at a given point
+  (3..dim).each do |n|
+    sum = square_sum(i, j, n, grid)
+    if sum > max
+      max = sum
+      pts = [i, j, n]
+    end
+  end
+
+  [pts, max]
+end
+
+def square_sum(i, j, dim, grid)
+  sum = 0
+  (i...i+dim).each do |x|
+    (j...j+dim).each do |y|
+
+      if x > 300-dim || y > 300-dim
+        next
+      end
+
+      sum += grid[x-1][y-1]
+    end
+  end
+  sum
+end
+
+serial = 3999
+grid = grid(serial)
+
 max = 0
 max_pts = nil
+max_square = nil
+max_square_sum = 10
+
+n = 300
 (1..n).each do |i|
   (1..n).each do |j|
-    if i+2 > 300 || j+2 > 300
-      # next
-    end
 
-    sum = power_level(i, j, serial) + power_level(i, j+1, serial) + power_level(i, j+2, serial) + power_level(i+1, j, serial) + power_level(i+1, j+1, serial) + power_level(i+1, j+2, serial) + power_level(i+2, j, serial) + power_level(i+2, j+1, serial) + power_level(i+2, j+2, serial)
+    sum = square_sum(i, j, 3, grid)
+    # ms, mss = max_square_sum(i, j, 70, grid)
+
+    # if mss > max_square_sum
+    #   max_square_sum = mss
+    #   max_square = ms
+    #   p [ms, mss]
+    # end
 
     if sum > max
       max = sum
       max_pts = [i, j]
     end
-
-    # grid[i][j] = sum
   end
 end
-p max, max_pts
 
-# p grid[20][61]
-# serial = 42
-# i = 21
-# j = 61
-# p power_level(i, j, serial)
-# p power_level(i, j+1, serial)
-# p power_level(i, j+2, serial)
-# puts
-# p power_level(i+1, j, serial)
-# p power_level(i+1, j+1, serial)
-# p power_level(i+1, j+2, serial)
-
-# p power_level(i+2, j, serial)
-# p power_level(i+2, j+1, serial)
-# p power_level(i+2, j+2, serial)
-
-# puts grid[21][61]
-# puts grid[20][61]
-# puts grid[20][62]
-# puts grid[21][60]
-# puts grid[21][61]
-# puts grid[21][62]
-# puts grid[22][60]
-# puts grid[22][61]
-# puts grid[22][62]
-
-# grid.each do |h, k|
-
-# pp grid
-# p power_level(3, 5, 8)
-# p power_level(122, 79, 57)
-# p power_level(217,196, 39)
-# p power_level(101,153, 71)
+p "Part 1: #{max}, #{max_pts}"
+p "Part 2: #{max_square_sum}, #{max_square}"
