8f1717817647d4d01659e25048d0bf026e31fb2e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-13 22:57:51 -0800

    cleanup

--- 2018/day14.rb
+++ 2018/day14.rb
@@ -1,14 +1,14 @@
 input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2018/inputs/day14.in")
-input = 84601
+input = "084601"
 
-scores = [3,7]
+scores = %w[3 7]
 i1, i2 = 0, 1
 
-n = input
+n = input.to_i
 loop do
-  r1, r2 = scores[i1], scores[i2]
+  r1, r2 = scores[i1].to_i, scores[i2].to_i
   r = r1+r2
-  score = r.to_s.chars.map { |x| x.to_i }
+  score = r.to_s.chars
   scores.concat score
   # p scores
 
@@ -16,7 +16,7 @@ loop do
   i2 = (r2 + i2 + 1) % scores.size
 
   if scores.size == n+10
-    p "Part 1: #{scores[n,n+9].map { |x| x.to_s }.join}"
+    p "Part 1: #{scores[n,n+9].join}"
     break
   end
 end
