887bfd1ec70534874d96ee524776c0645289474a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-14 13:28:28 -0800

    Day 14 (go)
    ~20 sec to run

--- 2018/go/day14/day14.go
+++ 2018/go/day14/day14.go
@@ -5,47 +5,71 @@ import (
 	"strconv"
 )
 
-
 func main() {
-	// input := "084601"
-	// input := []int{0,8,4,6,0,1}
-	input := "59414"
+	input := "084601"
 	n, _ := strconv.Atoi(input)
-	fmt.Println(n)
 
-	scores := []int{3, 7}
+	scores := map[int]int{
+		0: 3,
+		1: 7,
+	}
+
 	i1, i2 := 0, 1
+	next_index := 2
+
 	for {
-		// for i := 0; i < n; i++ {
 		r1, r2 := scores[i1], scores[i2]
 		score := r1 + r2
 		a := score / 10
 		b := score % 10
 		if a != 0 {
-			scores = append(scores, a)
-		}
-		scores = append(scores, b)
+			scores[next_index] = a
+			next_index++
 
-		i1 = (i1 + r1 + 1) % len(scores)
-		i2 = (i2 + r2 + 1) % len(scores)
+			if next_index >= len(input) {
+				n := next_index - len(input)
+				pattern := ""
+				for i := n; i < next_index; i++ {
 
-		if len(scores) >= len(input) {
-			n := len(scores) - len(input)
-			pattern := ""
-
-			for _, s := range scores[n:len(scores)] {
-				c := strconv.Itoa(s)
-				pattern = pattern +c
-				// fmt.Printf("pattern: %+v\n", pattern)
+					c := strconv.Itoa(scores[i])
+					pattern = pattern + c
+				}
+				if pattern == input {
+					fmt.Printf("Part 2 pattern: %+v\n", n)
+					break
+				}
 			}
+		}
+
+		scores[next_index] = b
+		next_index++
+
+		if next_index >= len(input) {
+			n := next_index - len(input)
+			pattern := ""
+			for i := n; i < next_index; i++ {
+				c := strconv.Itoa(scores[i])
+				pattern = pattern + c
+			}
+
 			if pattern == input {
-				fmt.Printf("Part 2 pattern: %+v\n", n)
+				fmt.Printf("Part 2: %+v\n", n)
 				break
 			}
-
 		}
+		i1 = (i1 + r1 + 1) % next_index
+		i2 = (i2 + r2 + 1) % next_index
 
+		// Part 1
+		if next_index == n+10 {
+			pattern := ""
+			for i := n; i < n+10; i++ {
+				c := strconv.Itoa(scores[i])
+				pattern = pattern + c
+			}
+			fmt.Printf("Part 1: %v\n", pattern)
+		}
 	}
-	// Part 1
-	// fmt.Println(scores[n:n+10])
 }
+// Part 1: 2688510125
+// Part 2 pattern: 20188250
