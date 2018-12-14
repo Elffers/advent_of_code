5c88c5a7009fcc6ffdc603026bbff91c8ea29d32
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-13 23:12:33 -0800

    Day 14 Part 2 very slow

--- 2018/day14.rb
+++ 2018/day14.rb
@@ -1,22 +1,40 @@
 input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2018/inputs/day14.in")
 input = "084601"
 
+input = "59414"
 scores = %w[3 7]
 i1, i2 = 0, 1
 
 n = input.to_i
+current = 0
 loop do
   r1, r2 = scores[i1].to_i, scores[i2].to_i
   r = r1+r2
+
   score = r.to_s.chars
   scores.concat score
-  # p scores
+
+  pattern = (current...current + input.size).map do |i|
+    scores[i]
+  end.join
+  # p pattern
+  if pattern == input
+    p "Part 2: #{current}"
+    break
+  else
+    current += 1
+  end
+
+
+  # if scores.join.include? input
+  #   p "found it"
+  # end
 
   i1 = (r1 + i1 + 1) % scores.size
   i2 = (r2 + i2 + 1) % scores.size
 
   if scores.size == n+10
     p "Part 1: #{scores[n,n+9].join}"
-    break
+    # break
   end
 end
