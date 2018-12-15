77572df244cf9312693982739b70a92a6a47d12e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2018-12-14 23:35:04 -0800

    Day 14 (ruby)
    real	1m11.624s
    user	1m10.948s
    sys	0m0.600s

--- 2018/day14.rb
+++ 2018/day14.rb
@@ -1,40 +1,57 @@
 input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2018/inputs/day14.in")
 input = "084601"
+# input = "59414"
+target = input.chars.map { |x| x.to_i }
+# input = "59414"
 
-input = "59414"
-scores = %w[3 7]
+scores = {
+  0 => 3,
+  1 => 7
+}
+
 i1, i2 = 0, 1
+i = 2 # next index
 
 n = input.to_i
-current = 0
 loop do
-  r1, r2 = scores[i1].to_i, scores[i2].to_i
+  r1, r2 = scores[i1], scores[i2]
   r = r1+r2
+  a = r/10
+  b = r % 10
 
-  score = r.to_s.chars
-  scores.concat score
+  if a != 0
+    scores[i] = a
 
-  pattern = (current...current + input.size).map do |i|
-    scores[i]
-  end.join
-  # p pattern
-  if pattern == input
-    p "Part 2: #{current}"
-    break
-  else
-    current += 1
+    i += 1 # increment index
+    j = i - target.size
+    pattern = (j...j + target.size).map do |idx|
+      scores[idx].to_s
+    end.join
+    if pattern == input
+      p "Part 2: #{j}"
+      break
+    end
   end
 
-
-  # if scores.join.include? input
-  #   p "found it"
-  # end
+  scores[i] = b
+  i += 1
+  j = i - target.size
+  pattern = (j...j + target.size).map do |idx|
+    scores[idx].to_s
+  end.join
+  if pattern == input
+      p "Part 2: #{j}"
+    break
+  end
 
   i1 = (r1 + i1 + 1) % scores.size
   i2 = (r2 + i2 + 1) % scores.size
 
   if scores.size == n+10
-    p "Part 1: #{scores[n,n+9].join}"
-    # break
+    pattern = (n..n+9).map do |idx|
+      scores[idx].to_s
+    end.join
+
+    p "Part 1: #{pattern}"
   end
 end
