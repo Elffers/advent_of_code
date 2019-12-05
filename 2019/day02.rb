2026ad8cf77d68ef2030af88a334c21f8be73e02
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-12-05 15:04:55 -0800

    Refactor IntCode Computer

--- 2019/day02.rb
+++ 2019/day02.rb
@@ -1,3 +1,5 @@
+require "./intcode.rb"
+
 input = File.read("./inputs/day02.in").strip
 
 def process input
@@ -6,39 +8,12 @@ end
 
 MEMORY = process input
 
-def run program, noun, verb
-  program[1] = noun
-  program[2] = verb
-
-  ip = 0
-
-  while program[ip] != 99
-    param1 = program[ip + 1]
-    param2 = program[ip + 2]
-    param3 = program[ip + 3]
-
-    val = 0
-    opcode = program[ip]
-
-    case opcode
-    when 1
-      val = program[param1] + program[param2]
-    when 2
-      val = program[param1] * program[param2]
-    end
-
-    program[param3] = val
-    ip += 4
-  end
-
-  program[0]
-end
-
 program = MEMORY.dup
 noun = 12
 verb = 2
 
-out = run program, noun, verb
+computer = Computer.new(program)
+out = computer.run noun, verb
 
 p "Part 1: #{out}"
 # 3306701
@@ -48,7 +23,8 @@ target = 19690720
 (0..99).each do |n|
   (0..99).each do |v|
     program = MEMORY.dup
-    out = run program, n, v
+    computer = Computer.new(program)
+    out = computer.run n, v
 
     if out == target
       p "Part 2: #{100 * n + v}"
---
+++ 2019/intcode.rb
@@ -0,0 +1,40 @@
+class Computer
+  attr_accessor :memory
+
+  def initialize memory
+    @memory = memory
+  end
+
+  def run noun, verb
+    boot noun, verb
+    ip = 0
+
+    while @memory[ip] != 99
+      param1 = @memory[ip + 1]
+      param2 = @memory[ip + 2]
+      param3 = @memory[ip + 3]
+
+      val = 0
+      opcode = @memory[ip]
+
+      case opcode
+      when 1
+        val = @memory[param1] + @memory[param2]
+      when 2
+        val = @memory[param1] * @memory[param2]
+      end
+
+      @memory[param3] = val
+      ip += 4
+    end
+
+    @memory[0]
+  end
+
+  private
+
+  def boot noun, verb
+    @memory[1] = noun
+    @memory[2] = verb
+  end
+end
