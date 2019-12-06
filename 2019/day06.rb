28636d9f6d6f3ba6e57e7e39cf127a24f9d9137a
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-12-06 14:21:37 -0800

    Day 6 part 2

--- 2019/day06.rb
+++ 2019/day06.rb
@@ -13,25 +13,26 @@ input = File.readlines("./inputs/day06.in").map { |x| x.strip }
 # E)J
 # J)K
 # K)L
+# K)YOU
+# I)SAN
 # ]
 
 graph = {}
+reverse = {}
 
 input.each do |line|
   x, y = line.split(')')
   graph[y] = x
+  reverse[x] = y
 end
 
 # leaves = graph.keys - (graph.keys & graph.keys)
-# roots = []
-# graph.each do |k, v|
-#   if !graph.keys.include? k
-#     roots << k
-#   end
-# end
-
-# p leaves
-# p roots
+root = []
+reverse.each do |k, v|
+  if !graph.keys.include? k
+    root = k
+  end
+end
 
 orbits = 0
 graph.each do |node, direct|
@@ -45,3 +46,21 @@ end
 
 p "Part 1: #{orbits}"
 # 453028
+
+# part 2 is find least common parent
+you_path = []
+node = "YOU"
+while !node.nil?
+  node = graph[node]
+  you_path << node
+end
+
+san_path = []
+node = "SAN"
+until you_path.include? node
+  node = graph[node]
+  san_path << node
+end
+# p "common ancestor: #{node}"
+
+p "Part 2: #{you_path.index(node) + san_path.index(node)}"
