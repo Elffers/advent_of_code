0170998330de91f59adedea1e5b62043853dfd28
Hsing-Hui Hsu <hhhsu1@gmail.com>
2019-12-08 00:23:16 -0800

    WIP day 7 part 2
    bug in intcode computer

--- 2019/day07.rb
+++ 2019/day07.rb
@@ -1,12 +1,12 @@
 require './computer.rb'
 
+# input = "./inputs/day07.in"
+# MEMORY = process input
+
 # input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 # input = "3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0"
-input = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+MEMORY = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".split(",").map { |x| x.to_i }
 
-input = "./inputs/day07.in"
-MEMORY = process input
 
 def process sequence
   signal = 0
@@ -21,11 +21,30 @@ def generate_signals
   sequences = [0, 1, 2, 3, 4].permutation(5).to_a
   sequences.map do |seq|
     process seq
-  end
+  end.max
+end
+
+def generate_max_signal
+  sequences = [5, 6, 7, 8, 9].permutation(5).to_a
+  sequences.map do |seq|
+    # init amps
+    amps = seq.map { |phase| Computer.new(MEMORY.dup, [phase]) }
+
+    signal = 0
+    while signal
+      out = signal
+      amps.each do |amp|
+        amp.receive out
+        signal = amp.run
+      end
+    end
+    out
+  end.max
 end
 
 if __FILE__ == $0
-  max = generate_signals.max
-  p "Part 1: #{max}"
+  p "Part 1: #{generate_signals}"
   # 199988
+
+  p "Part 2: #{generate_max_signal}"
 end
