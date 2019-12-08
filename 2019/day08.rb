41901a44f3fcd1f399b7b6c695a372fd8f24d0a9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2019-12-08 00:21:26 -0800

    Day 8 part 2

--- 2019/day08.rb
+++ 2019/day08.rb
@@ -1,14 +1,39 @@
-require 'pp'
+pwd = File.read("./inputs/day08.in").strip
+ROWS = 6
+COLS = 25
 
-pwd = File.read("/Users/hhh/JungleGym/advent_of_code/2019/inputs/day08.in").strip
+dim = ROWS*COLS
 
-dim = 25*6
 counts = {}
+layers = []
 
 pwd.chars.each_slice(dim) do |layer|
   count = layer.count("0")
-  counts[count] = layer.join
+  counts[count] = layer
+  layers << layer
 end
 
-target = counts.keys.min
-p "Part 1: #{counts[target].count("1")* counts[target].count("2")}"
+min = counts.keys.min
+p "Part 1: #{counts[min].count("1")* counts[min].count("2")}"
+# 2904
+
+def print_layer layer
+  layer.each_slice(COLS) do |row|
+    x = row.map {|i| i == "0" ? " " : "*" }
+    puts x.join
+  end
+end
+
+# Part 2
+out = (0...dim).map do |i|
+  n = 0
+  pxl = layers[n][i]
+  while pxl == "2"
+    n += 1
+    pxl = layers[n][i]
+  end
+  pxl
+end
+
+print_layer out
+# HGBCF
