c8d0d0edb32fc60851943b13a0f314e663dbfee5
Hsing-Hui Hsu <hhhsu1@gmail.com>
2019-12-08 23:14:01 -0800

    Day 7 part 2

--- 2019/computer.rb
+++ 2019/computer.rb
@@ -1,9 +1,11 @@
 class Computer
-  attr_accessor :memory, :inputs, :output
+  attr_accessor :memory, :inputs, :output, :ip
 
   def initialize memory, inputs
     @memory = memory
     @inputs = inputs # List of input values
+    @phase = inputs.first
+    @ip = 0
   end
 
   def receive signal
@@ -11,52 +13,48 @@ class Computer
   end
 
   def run
-    ip = 0
     jump = 0
 
     while @memory[ip] != 99
-      # Initialize instruction
-      # NOTE: Pad this with two 0's only, since this is only needed for opcode 1
-      # and 2 where the 3rd parameter is always one that is written to so
-      # therefore always has param mode 0
-      # instr = sprintf("%04d", @memory[ip])
       instr = @memory[ip]
       opcode = instr % 100
-      # param_modes = [instr[1], instr[0]] # instr is read right to left
 
-      param1 = (instr/100) % 10 == 0 ? @memory[@memory[ip+1]] : @memory[ip+1]
-      param2 = (instr/1000) % 10 == 0 ? @memory[@memory[ip+2]] : @memory[ip+2]
-      param3 = (instr/10000) % 10 == 0 ? @memory[@memory[ip+3]] : @memory[ip+3]
-
-      p "Params: #{param1}, #{param2}, #{param3}"
-      p "Instr: #{instr}"
+      param1 = (instr/100)%10 == 0 ? @memory[@memory[ip+1]] : @memory[ip+1] # rescue nil
+      param2 = (instr/1000)%10 == 0 ? @memory[@memory[ip+2]] : @memory[ip+2] #rescue nil
 
       case opcode
       when 1
-        @memory[param3] = param1 + param2
+        addr = @memory[ip+3]
+        @memory[addr] = param1 + param2
         jump = 4
 
       when 2
-        @memory[param3] = param1 * param2
+        addr = @memory[ip+3]
+
+        @memory[addr] = param1 * param2
         jump = 4
 
       when 3
         addr = @memory[ip + 1]
+        if @inputs.empty?
+          return @output
+        end
+
         @memory[addr] = @inputs.shift
         jump = 2
 
       when 4
-          @output = param1
-        p "Write instruction output: #{@output}"
-
-        jump = 2
+        @ip += 2
+        @output = param1
+        return @output
+        # p "Write instruction output: #{@output}"
 
       when 5
         # Opcode 5 is jump-if-true: if the first parameter is non-zero, it
         # sets the instruction pointer to the value from the second parameter.
         # Otherwise, it does nothing.
           if param1 != 0
-            ip = params[1]
+            @ip = param2
             jump = 0
           else
             jump = 3
@@ -67,7 +65,7 @@ class Computer
         # the instruction pointer to the value from the second parameter.
         # Otherwise, it does nothing.
           if param1 == 0
-            ip = params[1]
+            @ip = param2
             jump = 0
           else
             jump = 3
@@ -77,10 +75,11 @@ class Computer
         # Opcode 7 is less than: if the first parameter is less than the
         # second parameter, it stores 1 in the position given by the third
         # parameter. Otherwise, it stores 0.
-        if param1 < params2
-          @memory[param3] = 1
+        addr = @memory[ip + 3]
+        if param1 < param2
+          @memory[addr] = 1
         else
-          @memory[param3] = 0
+          @memory[addr] = 0
         end
         jump = 4
 
@@ -88,38 +87,22 @@ class Computer
         # Opcode 8 is equals: if the first parameter is equal to the second
         # parameter, it stores 1 in the position given by the third parameter.
         # Otherwise, it stores 0.
+        addr = @memory[ip + 3]
 
         if param1 == param2
-          @memory[param3] = 1
+          @memory[addr] = 1
         else
-          @memory[param3] = 0
+          @memory[addr] = 0
         end
         jump = 4
       else
         p "ERROR: unknown instruction: #{instr}"
       end
 
-      ip += jump
+      @ip += jump
     end
-    @output
   end
 end
-
-# def get_params params, param_modes
-#   out = []
-
-#   params.each_with_index do |param, i|
-#     if param_modes[i] == "0"
-#       out[i] = @memory[param]
-#     elsif param_modes[i] == "1"
-#       out[i] = param
-#     else
-#       p "Unrecognized parameter mode"
-#     end
-#   end
-
-#   out
-# end
 
 class IO
   attr_reader :id
--- 2019/day07.rb
+++ 2019/day07.rb
@@ -1,11 +1,11 @@
 require './computer.rb'
 
-# input = "./inputs/day07.in"
-# MEMORY = process input
+input = "./inputs/day07.in"
+MEMORY = process input
 
 # input = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 # input = "3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0"
-MEMORY = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".split(",").map { |x| x.to_i }
+# MEMORY = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".split(",").map { |x| x.to_i }
 
 
 def process sequence
@@ -30,15 +30,17 @@ def generate_max_signal
     # init amps
     amps = seq.map { |phase| Computer.new(MEMORY.dup, [phase]) }
 
-    signal = 0
-    while signal
-      out = signal
+    out = signal = 0
+
+    while out
+      signal = out
       amps.each do |amp|
         amp.receive out
-        signal = amp.run
+        out = amp.run
       end
     end
-    out
+
+    signal
   end.max
 end
 
@@ -47,4 +49,5 @@ if __FILE__ == $0
   # 199988
 
   p "Part 2: #{generate_max_signal}"
+  # 17519904
 end
