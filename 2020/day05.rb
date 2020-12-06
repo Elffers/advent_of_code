6b0bf7a06e916d2c17c50112d6478261b1148975
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-06 10:08:12 -0800

    cleanup day 5

--- 2020/day05.rb
+++ 2020/day05.rb
@@ -1,11 +1,8 @@
-require 'pp'
 input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day05.in").map { |x| x.strip }
 
 # map each line to binary representation
 seats = input.map do |x|
-  row = x[0,7].tr("FB", "01")
-  col =  x[7,10].tr("LR", "01")
-  row + col
+  x.tr("FBLR", "0101")
 end
 
 # binary rep already is the same thing as multiplying row by 8 and adding column,
@@ -13,14 +10,14 @@ end
 id = seats.max
 p "part 1: #{id.to_i(2)}"
 
+# to find missing seat, find which of the sorted list does not correspond to
+# index plus offset of first seat
 sorted = seats.map do |b|
   b.to_i(2)
 end.sort
 
 offset = sorted.first
 
-# to find missing seat, find which of the sorted list does not correspond to
-# index plus offset of first seat
 sorted.each_with_index do |s, i|
   if s != i + offset
     p "part 2: #{s - 1}"
