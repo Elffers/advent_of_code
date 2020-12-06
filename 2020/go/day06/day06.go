2c8d99f600ed0a73fb052d54475cb3915a14d3d7
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-06 11:07:28 -0800

    day6 pt 2 go

--- 2020/go/day06/day06.go
+++ 2020/go/day06/day06.go
@@ -13,15 +13,23 @@ func main() {
 	}
 	groups := strings.Split(string(input), "\n\n")
 
-	p1 := 0
+	p1, p2 := 0, 0
 
 	for _, g := range groups {
-		unique := make(map[rune]bool)
-		g = strings.ReplaceAll(g, "\n", "")
-		for _, c := range g {
-			unique[c] = true
+		unique := make(map[rune]int)
+		s := strings.ReplaceAll(g, "\n", "")
+		for _, c := range s {
+			unique[c] += 1
 		}
 		p1 += len(unique)
+
+		size := len(strings.Split(g, "\n"))
+		for _, count := range unique {
+			if count == size {
+				p2 += 1
+			}
+		}
 	}
-	fmt.Println(p1)
+	fmt.Printf("part 1: %d\n", p1)
+	fmt.Printf("part 2: %d\n", p2)
 }
