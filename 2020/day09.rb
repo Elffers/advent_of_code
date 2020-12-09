766eeb31c1d692a26072428e99168340284c55af
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-08 21:39:40 -0800

    day 9 pt 2

--- 2020/day09.rb
+++ 2020/day09.rb
@@ -1,16 +1,12 @@
 require 'set'
 
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day09.in").split("\n").map { |x| x.to_i }
-# input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day09test.in").split("\n").map { |x| x.to_i }
 
 x, y = 0, 25
-# x, y = 0, 5 # test
 window = input[x,y]
 queue = input[y, input.size]
-# p queue.size
 
 def sum_in? n, window
-
   set = Set.new(window)
 
   set.any? do |x|
@@ -22,10 +18,32 @@ end
 while !queue.empty?
   curr = queue.shift
   if !sum_in? curr, window
-    p curr
+    p "part 1: #{curr}"
     break
   end
   window.shift
   window << curr
+end
+
+target = curr
+
+x = 0
+y = x + 2
+
+while y < input.size
+  window = input[x..y]
+  # p window
+  sum = window.reduce(:+)
+  if sum == target
+    p "part 2: #{window.min + window.max}"
+    break
+  end
+  if sum < target
+    y += 1
+  end
+  if sum > target
+    x += 1
+    y = x + 1
+  end
 end
 
