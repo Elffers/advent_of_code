c5b8a3658021faa4c1f7e6d1d120f9ede96406a8
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-09 23:43:19 -0800

    day 10 pt 2

--- 2020/day10.rb
+++ 2020/day10.rb
@@ -1,25 +1,66 @@
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day10.in").split("\n").map { |x| x.to_i }
-# input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day10test.in").split("\n").map { |x| x.to_i }
+adapters = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day10.in").split("\n").map { |x| x.to_i }
 
-device = input.max + 3
-input << device
-input << 0
-p device
+device = adapters.max + 3
+adapters << device
+adapters << 0
+
 d1, d3 = 0,0
 
-adapters = input.sort
-
-adapters.each_cons(2) do |pair|
-  p pair
-  x, y = pair
+adapters.sort.each_cons(2) do |x, y|
   diff = y-x
 
   d1 += 1 if diff == 1
   d3 += 1 if diff == 3
-  if !(diff == 1) && !(diff == 3)
-    p "error: #{pair}"
-  end
 end
-p "counts: #{[d1, d3]}"
-p d1 * d3
+
+p "part 1: #{d1 * d3}"
+
+# NOTE: Traditional DFS is too slow
+#
+# graph = Hash.new { |h, k| h[k] = [] }
+# adapters.each do |x|
+#   choices = [x+1, x+2, x+3]
+#   choices.each do |choice|
+#     if adapters.include? choice
+#       graph[x] << choice
+#     end
+#   end
+# end
+# p graph
+
+# target = device
+# queue = graph[0]
+# combos = 0
+
+# # dfs
+# while !queue.empty?
+#   curr = queue.shift
+#   if curr == target
+#     combos += 1
+#   else
+#     queue.concat(graph[curr])
+#   end
+# end
+
+# p "part 2: #{combos}"
+
+
+# Use dynamic programming to cache the number of paths from each joltage node.
+# This will recursively populate the cache bottom-up for each adapter with the
+# number of paths to the terminus, which will be the sum of the paths to the
+# terminus for each child node.
+
+combos = Hash.new do |paths, j|
+  choices = [j+1, j+2, j+3]
+  child_adapters = choices.select { |c| adapters.include? c }
+
+  # sum of number of paths for each child node
+  count = child_adapters.map { |a| paths[a] }.sum
+  paths[j] = count
+end
+
+# at the terminus, there is one path
+combos[device] = 1
+
+p "part 2: #{combos[0]}"
 
