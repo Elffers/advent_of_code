db536a8674cbc30366f6c14f962fb60e93fb90db
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-13 22:01:51 -0800

    day 14 pt 2

--- 2020/day14.rb
+++ 2020/day14.rb
@@ -1,25 +1,66 @@
+require 'pp'
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day14.in").split("\n")
 
 mem = Hash.new 0
 mem2 = Hash.new 0
 
-mask = "0" * 36
-input.each do |x|
-  instr, val = x.split(" = ")
-  if instr == "mask"
-    mask = val
-  else
-    /mem\[(?<addr>\d+)\]/ =~ instr
-    val = ("%036b" % val.to_i).chars.zip(mask.chars).map do |b, m|
-      if m == "X"
-        b
+def mask_bits(bits, mask)
+  ("%036b" % bits.to_i).chars.zip(mask.chars).map do |b, m|
+    if m == "X"
+      b
+    else
+      m
+    end
+  end.join.to_i(2)
+end
+
+# returns list of all masked addresses
+def masked_addrs(addr, mask)
+  floats = mask.count("X")
+
+  bin = ("%036b" % addr.to_i).chars.zip(mask.chars).map do |b, m|
+    if m == "0"
+      b
+    else
+      m
+    end
+  end
+
+  bit_combos = (0...2**floats).map do |float|
+    ("%0#{floats}b" % float).chars
+  end
+
+  bit_combos.map do |combo|
+    addr = bin.dup
+    addr.map.with_index do |b, i|
+      if b == "X"
+        addr[i] = combo.shift
       else
-        m
+        b
       end
     end.join.to_i(2)
+  end
+end
+
+mask = "0" * 36
+input.each do |x|
+  instr, bits = x.split(" = ")
+  if instr == "mask"
+    mask = bits
+  else
+    /mem\[(?<addr>\d+)\]/ =~ instr
+
+    val = mask_bits(bits, mask)
 
     mem[addr] = val
+
+    # part 2
+    addrs = masked_addrs(addr, mask)
+    addrs.each do |a|
+      mem2[a] = bits.to_i
+    end
   end
 end
 
 p "part 1: #{mem.values.sum}"
+p "part 2: #{mem2.values.sum}"
