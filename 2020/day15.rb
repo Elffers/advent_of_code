dc3bf3ee4050050f6b095046dc7ec83eb8341521
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-14 22:23:52 -0800

    day 15 refactor

--- 2020/day15.rb
+++ 2020/day15.rb
@@ -1,32 +1,37 @@
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day15.in").split(",").map { |x| x.to_i }
+# input = [0,3,6]
 
-# holds each number and the last turn it was spoken
-game = {}
+def run input, n
+  # holds each number and the last turn it was spoken
+  last_turn = {}
 
-turns = 0
-input[0...input.size-1].each_with_index do |x, i|
-  game[x] = i+1
-  turns = i+1
+  # populate map with everything in input except last element
+  input[0...input.size-1].each_with_index do |x, i|
+    last_turn[x] = i+1
+  end
+
+  curr = input.last
+  turn = input.size
+
+  while turn < n
+    if last_turn[curr].nil?
+      last_turn[curr] = turn
+      curr = 0
+    else
+      prev = last_turn[curr]
+      last_turn[curr] = turn
+      curr = turn - prev
+    end
+    turn += 1
+  end
+
+  curr
 end
 
-last = input.last
 
-loop do
-  turns += 1
-  if game[last].nil?
-    game[last] = turns
-    last = 0
-  else
-    last_turn = game[last]
-    game[last] = turns
-    last = turns - last_turn
-  end
+res = run input, 2020
+p "part 1: #{res}"
 
-  if turns == 2020-1
-     p "part 1: #{last}"
-  end
-  if turns == 30000000-1
-     p "part 2: #{last}"
-     break
-  end
-end
+res = run input, 30000000
+p "part 2: #{res}"
+
