6d100c9aa58dcb3962c34f6da88345f7ad577f3b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-17 23:33:49 -0800

    cleanup

--- 2020/day18.rb
+++ 2020/day18.rb
@@ -3,7 +3,6 @@ input = File.read("/Users/hhh/JungleGym/advent_of_code/2020/inputs/day18.in").sp
 # evaluates a complete expression within a single set of parens
 def expression expr
   stack = []
-  p "input: #{expr.join}"
 
   while !expr.empty?
     curr = expr.shift
@@ -23,18 +22,11 @@ def expression expr
       stack << res
     end
   end
-  p "output: #{ stack}"
   stack.first
-end
-
-# %[a b]
-def addition expr
-  expr.first.to_i + expr.last.to_i
 end
 
 def advanced_math expr
   stack = []
-  p "input: #{expr.join}"
 
   while !expr.empty?
     curr = expr.shift
@@ -53,7 +45,6 @@ def advanced_math expr
 end
 
 def calculate chars
-  p chars.join
   stack = []
   while !chars.empty?
     curr = chars.shift
@@ -72,30 +63,15 @@ def calculate chars
         expr.unshift x
       end
       stack.pop # get rid of opening (
+      # res = expression expr # part 1
       res = advanced_math expr
 
-      p "expr: #{expr}, stack before: #{stack.join}"
       stack.push res.to_s
-      p "stack after: #{stack.join}"
     end
 
-    p "stack: #{stack.join}"
   end
-  res = advanced_math(stack)
-  res
+  # expression(stack) # part 1
+  advanced_math(stack)
 end
 
-# line = "1 + 2 * 3 + 4 * 5 + 6"
-# line = "1 + (2 * 3) + (4 * (5 + 6))"
-line = "2 * 3 + (4 * 5)"
-
-line = "5 + (8 * 3 + 9 + 3 * 4 * 3)"
-p calculate(line.delete(" ").chars)
-# line = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
-line = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
-
-# p line
-# p calculate(line.delete(" ").chars)
-
-res = input.map { |line| calculate(line.chars) }.reduce(:+)
-p "part 2: #{res}"
+p input.map { |line| calculate(line.chars) }.reduce(:+)
