f0c5f445465b3dff1f425ae7f00090dc8347765f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-12-17 23:32:21 -0800

    day 18 pt 2

--- 2020/day18.rb
+++ 2020/day18.rb
@@ -27,6 +27,31 @@ def expression expr
   stack.first
 end
 
+# %[a b]
+def addition expr
+  expr.first.to_i + expr.last.to_i
+end
+
+def advanced_math expr
+  stack = []
+  p "input: #{expr.join}"
+
+  while !expr.empty?
+    curr = expr.shift
+    case curr
+    when /(\d+)/
+      stack << $1
+    when "+"
+      x = stack.pop.to_i
+      y = expr.shift.to_i
+      stack << (x+y).to_s
+    when "*"
+      stack << curr
+    end
+  end
+  expression(stack)
+end
+
 def calculate chars
   p chars.join
   stack = []
@@ -47,8 +72,7 @@ def calculate chars
         expr.unshift x
       end
       stack.pop # get rid of opening (
-      res = expression expr
-      p "res: #{res}"
+      res = advanced_math expr
 
       p "expr: #{expr}, stack before: #{stack.join}"
       stack.push res.to_s
@@ -57,20 +81,21 @@ def calculate chars
 
     p "stack: #{stack.join}"
   end
-res = expression(stack)
-res
+  res = advanced_math(stack)
+  res
 end
 
 # line = "1 + 2 * 3 + 4 * 5 + 6"
 # line = "1 + (2 * 3) + (4 * (5 + 6))"
-# line = "2 * 3 + (4 * 5)"
+line = "2 * 3 + (4 * 5)"
 
-# line = "5 + (8 * 3 + 9 + 3 * 4 * 3)"
+line = "5 + (8 * 3 + 9 + 3 * 4 * 3)"
+p calculate(line.delete(" ").chars)
 # line = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
 line = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
 
 # p line
-p calculate(line.delete(" ").chars)
+# p calculate(line.delete(" ").chars)
 
 res = input.map { |line| calculate(line.chars) }.reduce(:+)
-p "part 1: #{res}"
+p "part 2: #{res}"
