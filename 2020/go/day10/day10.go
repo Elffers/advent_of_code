a1c9803a549887b1f8b31f7796e427f40cf521c3
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-01-07 23:40:31 -0800

    day 10 go naive

--- 2020/go/day10/day10.go
+++ 2020/go/day10/day10.go
@@ -32,17 +32,57 @@ func main() {
 	}
 	adapters := format(string(input))
 
+	d1, d3 := 0, 0
+
+	prev := adapters[0]
+
+	set := make(map[int]bool) // for part 2
+
+	for i, a := range adapters {
+		set[a] = true
+		if i == 0 {
+			continue
+		}
+		diff := a-prev
+		if diff == 1 {
+			d1 += 1
+		}
+		if diff == 3 {
+			d3 += 1
+		}
+		prev = a
+	}
+
+	fmt.Printf("part 1: %v\n", d1*d3)
+
 	graph := make(map[int][]int)
 
 	for _, a := range adapters {
 		choices := []int{a+1, a+2, a+3}
+		children := []int{}
 		for _, c := range choices {
-			if 
-			graph[a] 
+			if _, ok := set[c]; ok {
+				children = append(children, c)
+			}
 		}
+		graph[a] = children
 	}
 
 
-	fmt.Printf("part 1: %v\n", adapters )
+	target := adapters[len(adapters)-1]
+	queue := graph[0]
+	combos := 0
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		if curr == target {
+			combos += 1
+		} else {
+			queue = append(queue, graph[curr]...)
+		}
+	}
+
+	fmt.Printf("part 2: %v\n", combos)
 }
 
