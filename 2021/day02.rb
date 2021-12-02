ee595600cf3414f2dadfa238f1accaf8177a01ba
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-01 21:12:08 -0800

    day2b

--- 2021/day02.rb
+++ 2021/day02.rb
@@ -1,16 +1,24 @@
-input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day02.in").map { |x| x.strip }
+input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day02.in").split("\n")
+
 x, y = 0, 0
+x2, y2, a = 0, 0, 0
+
 input.each do |i|
-  /(?<dir>\w+) (?<d>\d+)/ =~ i
+  dir, d = i.split " "
+  d = d.to_i
   case dir
   when "forward"
-    x += d.to_i
+    x += d
+    x2 += d
+    y2 += a*d
   when "down"
-    y += d.to_i
+    y += d
+    a += d
   when "up"
-    y -= d.to_i
+    y -= d
+    a -= d
   end
 end
 
-p x*y
-
+p "Part 1: #{x*y}"
+p "Part 2: #{x2*y2}"
