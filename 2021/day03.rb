3db7618ad4b9b8d955767c8944421fe0fda17502
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-02 22:04:14 -0800

    day3b

--- 2021/day03.rb
+++ 2021/day03.rb
@@ -1,7 +1,8 @@
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day03.in").split("\n").map { |x| x.chars }.transpose
+input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day03.in").split("\n")
+
 g = ""
 e = ""
-input.map do |x|
+input.map { |i| i.chars}.transpose.map do |x|
   a = x.count { |i| i == "1" }
   b = x.count { |i| i == "0" }
   if a > b
@@ -12,6 +13,30 @@ input.map do |x|
     e += "1"
   end
 end
+p "Part 1: #{[g, e].map { |x| x.to_i(2) }.reduce(:*)}"
 
-p [g, e].map { |x| x.to_i(2) }.reduce(:*)
+def find_oxy xs, pos
+  return xs.first if xs.length == 1
+  bits = xs.map { |x| x[pos] }
+  x = bits.count "1"
+  y = bits.count "0"
+  b = if x >= y then "1" else "0" end
+  xs = xs.select { |n| n[pos] == b }
+  pos += 1
+  find_oxy xs, pos
+end
 
+def find_co2 xs, pos
+  return xs.first if xs.length == 1
+  bits = xs.map { |x| x[pos] }
+  x = bits.count "1"
+  y = bits.count "0"
+  b = if x < y then "1" else "0" end
+  xs = xs.select { |n| n[pos] == b }
+  pos += 1
+  find_co2 xs, pos
+end
+
+oxy = find_oxy input, 0
+co2 = find_co2 input, 0
+p "Part 2: #{[oxy, co2].map { |x| x.to_i(2) }.reduce(:*)}"
