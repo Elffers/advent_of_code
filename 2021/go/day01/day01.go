501d16e4ad97e1988d4d9fd9db8b1ea0629c8dd8
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-04 17:14:22 -0800

    day1b go

--- 2021/go/day01/day01.go
+++ 2021/go/day01/day01.go
@@ -7,22 +7,43 @@ import(
 	"strconv"
 )
 
+func countIncreases(nums []int) int {
+	count := 0
+	for i, n := range nums {
+		if i == 0 {
+			continue
+		}
+		if n-nums[i-1] > 0 {
+			count += 1
+		}
+	}
+	return count
+}
+
+func countWindowIncreases(nums []int) int {
+	count := 0
+	prevSum := nums[0] + nums[1] + nums[2]
+	for i, _ := range nums {
+		if i == 0 || i == 1 || i == 2 {
+			continue
+		}
+	currSum := nums[i] + nums[i-1] + nums[i-2]
+		if currSum-prevSum > 0 {
+			count += 1
+		}
+	prevSum = currSum
+	}
+	return count
+}
+
 func main() {
 	input, err := ioutil.ReadFile("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day01.in")
 	if err != nil {
 		panic(err)
 	}
 	nums := inputToIntSlice(string(input))
-	count := 0
-	for i, s := range nums {
-		if i == 0 {
-			continue
-		}
-		if s-nums[i-1] > 0 {
-			count += 1
-		}
-	}
-	fmt.Print(count)
+	fmt.Printf("Part 1: %+v\n", countIncreases(nums))
+	fmt.Printf("Part 2: %+v\n", countWindowIncreases(nums))
 }
 
 func inputToIntSlice(input string) []int {
@@ -37,4 +58,3 @@ func inputToIntSlice(input string) []int {
 	}
 	return out
 }
-
