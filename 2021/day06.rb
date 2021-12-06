5ae678fcf83d6d443bf044691485995afed15df0
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-05 23:21:14 -0800

    day6b

--- 2021/day06.rb
+++ 2021/day06.rb
@@ -1,37 +1,28 @@
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day06test.in").split(",").map { |x| x.to_i }
-# input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day06.in").split(",").map { |x| x.to_i }
+input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day06.in").split(",").map { |x| x.to_i }
 
-def tick input
-  out = Array.new(input.size + input.count(0))
-  ni = input.size
-  input.each_with_index do |x,i|
-    if x == 0
-      out[i] = 6
-      out[ni] = 8
-      ni += 1
+# keeps track of how many fish there are with a particular internal clock value from 0-8 (corresponds to index)
+counts = Hash.new 0
+input.each do |x|
+  counts[x] += 1
+end
+
+def tick counts
+  nc = Hash.new 0
+  counts.each do |k, v|
+    if k == 0
+      nc[6] += v
+      nc[8] += v
     else
-      out[i] = x-1
+      nc[k-1] += v
     end
   end
-  out
+  nc
 end
 
-x = [4] 
-30.times do |i|
-  x = tick x
-  p [i+1, x.length]
+256.times do |i|
+  counts = tick counts
+  if i == 79 
+    p "Part 1: #{counts.values.sum}"
+  end
 end
-p "Part 1: #{x.length}"
-
-# x = input
-# 256.times do |i|
-#   p [i, x.length]
-#   x = tick x
-# end
-
-# p x.length
-# def spawn clock, days
-#   if days = 0
-#     return 1
-#   end
-# end
+p "Part 2: #{counts.values.sum}"
