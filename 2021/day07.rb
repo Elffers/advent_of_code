a9c1e15d1923fdfc06c400c2d000462cd3052c38
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-06 21:47:19 -0800

    day7b

--- 2021/day07.rb
+++ 2021/day07.rb
@@ -1,11 +1,11 @@
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day07test.in").split(",").map { |x| x.to_i }
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day07.in").split(",").map { |x| x.to_i }
+
 x, y = input.minmax
-p [x,y]
 
 ans = (x..y).map do |i|
-  s = input.map do |p|
-    (p-i).abs
+  fuel = input.map do |p|
+    d = (p-i).abs # just this for part 1
+    (1..d).sum # part 2
   end.sum
 end.min
 
