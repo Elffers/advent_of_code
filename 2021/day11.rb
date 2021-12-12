d01cf2c04bf634b2f7ec477678a89f1b71908089
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-11 19:17:43 -0800

    day11a

--- 2021/day11.rb
+++ 2021/day11.rb
@@ -1,28 +1,26 @@
+require 'set'
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day11test.in").split("\n")
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day11test2.in").split("\n")
-# input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day11.in").split("\n")
+input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day11.in").split("\n")
 
 @h = input.size
 @w = input.first.size
 
-# os = input.map { |l| l.chars.map { |x| x.to_i }}
-#
-
 def neighbors x, y
   dirs = [
-  [-1, -1], [-1, 0], [-1, 1],
-  [ 0, -1],          [ 0, 1],
-  [ 1, -1], [ 1, 0], [ 1, 1],
-]
-dirs.map do |(i,j)|
-  dx, dy = x+i, y+j
-  if (0 <= dx  && dx < @h) && (0 <= dy && dy < @w)
-    [dx, dy]
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1],          [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1],
+  ]
+  ns = []
+  dirs.each do |(i,j)|
+    dx, dy = x+i, y+j
+    if (0 <= dx  && dx < @h) && (0 <= dy && dy < @w)
+      ns << [dx, dy]
+    end
   end
-   end
+  ns
 end
 
-# everyone increases by 1
 def inc octs
   octs.map do |line|
     line.map do |n|
@@ -31,58 +29,36 @@ def inc octs
   end
 end
 
-def flash os
-  flashes = 0
-  fs = os.dup
-  # anyone > 9 increases all neighbors by 1
+def flash x, y, input
+  # @flashed << [x, y]
+  @flashes += 1
+  input[x][y] = 0 # flashed
+  ns = neighbors x, y
+  # increment neighbors
+  ns.each do |(i,j)|
+    if input[i][j] != 10 && input[i][j] != 0
+      input[i][j] += 1
+      if input[i][j] == 10
+        flash i, j, input
+      end
+    end
+  end
+end
+
+@flashes = 0
+os = input.map { |x| x.chars.map { |c| c.to_i }}
+100.times do
+  os = inc os
   os.each_with_index do |line, x|
     line.each_with_index do |o, y|
-      if o > 9
-        ns = neighbors x, y
-        ns.each do |(i,j)|
-          fs[i][j] += 1
-        end
-        #need to find out how much to increment this thing by based on cascading
-        # queue = ns
-        # while !queue.empty?
-        #   n = queue.shift # coordinates
-        #   i, j = n.first, n.last
-        #   fs[i][j] += 1
-        #   if fs[i][j] > 9
-        #     # fs[i][j] =0
-        #     queue.concat neighbors(i,j)
-        #   end
-        # end
+      if o == 10
+        flash x, y, os
       end
     end
   end
-
-  xs = fs.dup
-  fs.each_with_index do |line, x|
-    line.each_with_index do |p, y|
-      if p > 9
-        xs[x][y] = 0
-        flashes += 1
-      end
-    end
-  end
-
-  p "FLASH"
-  xs.each { |o| p o}
-  puts
-
-  [xs, flashes]
+  # p "FLASH"
+  # os.each { |o| p o }
+  # puts
 end
 
-
-flashes = 0
-octs = input.map { |x| x.chars.map { |c| c.to_i }}
-10.times do
-  os = inc octs
-  os.each { |o| p o}
-  puts
-  octs, f  = flash os
-  flashes += f
-end
-
-p "Part 1: #{flashes}"
+p "Part 1: #{@flashes}"
