1add366441a7219ec42c6494b61bea70f757bcfb
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-12 20:43:33 -0800

    day 12b

--- 2021/day12.rb
+++ 2021/day12.rb
@@ -7,14 +7,13 @@ input.each do |l|
   @map[b] << a
 end
 
-@paths = []
-def path_from s, path, seen
+def path_from s, path, seen, can_visit
   path << s
   if s.match?(/[a-z]/)
     seen[s] += 1
   end
   if s == "end"
-    puts "PATH: #{path.join ","}"
+    # puts "PATH: #{path.join ","}"
     @paths << path
     return
   end
@@ -22,18 +21,43 @@ def path_from s, path, seen
   ns = @map[s]
   if !ns.nil?
     ns.each do |c|
-      if !seen.include? c
-        s = seen.dup
+      if can_visit.call c, seen
+        v = seen.dup
         p = path.dup
-        path_from c, p, s
+        path_from c, p, v, can_visit
       end
     end
+  end
+  if s.match?(/[a-z]/)
+    seen[s] -= 1
   end
   path.pop
 end
 
+@paths = []
 seen = Hash.new 0
 path = []
-path_from "start", path, seen
+path_from "start", path, seen, lambda { |x, v| !v.include? x }
 
 p "Part 1: #{@paths.count}"
+
+def can_visit
+  lambda do |s, seen|
+    if s.match?(/[A-Z]/)
+      true
+    elsif s == "start"
+      false
+    elsif seen.values.max > 1
+      seen[s] == 0
+    else
+      seen[s] < 2
+    end
+  end
+end
+
+@paths = []
+seen = Hash.new 0
+path = []
+path_from "start", path, seen, can_visit
+
+p "Part 2: #{@paths.count}"
