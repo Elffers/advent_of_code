1c3735bb5094fd4adc91eed19bcdd879240e8164
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-13 22:17:43 -0800

    cleanup

--- 2021/day14.rb
+++ 2021/day14.rb
@@ -1,4 +1,3 @@
-input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day14test.in").split("\n\n")
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day14.in").split("\n\n")
 
 template = input.first
@@ -7,23 +6,6 @@ input.last.split("\n").each do |x|
   p = x.split " -> "
   @rules[p.first] = p.last
 end
-
-def insert template
-  counts = Hash.new 0
-  cs = template.chars
-  cs.each do |c|
-    counts[c] += 1
-  end
-  new = ""
-  cs.each_cons(2) do |(a,b)|
-    v = @rules[a+b]
-    new += a + v
-    counts[v] += 1
-  end
-  new + template.chars.last
-end
-
-# part 2
 
 def count_pairs pcs, last
   pair_counts = Hash.new 0
@@ -36,45 +18,36 @@ def count_pairs pcs, last
     y = pair[1]
     cts[c] += v
     cts[x] += v
+
     new_pair = x + c
     pair_counts[new_pair] += v
     new_pair = c + y
     pair_counts[new_pair] += v
   end
-  puts "COUNTS: #{cts.values.max - cts.values.min}"
-  pair_counts
+  [pair_counts, cts]
 end
 
 pairs = Hash.new 0
+counts = Hash.new 0
 cs = template.chars
+
 cs.each_cons(2) do |p|
   pairs[p.join] += 1
 end
 
-counts = Hash.new 0
 cs.each do |c|
   counts[c] += 1
 end
 
-p "START"
-p pairs, counts
-puts
-x = template.chars.last
-40.times do
-  pairs, count = count_pairs pairs, x
-  # p pairs
-  # puts
-end
-
 i = 0
-# 10.times do
-#   template = insert template
-#   i += 1
-#   p [i, template.size]
-# end
-
-# count = Hash.new 0
-# template.chars.each do |c|
-#   count[c] += 1
-# end
-# p count.values.max - count.values.min
+x = template.chars.last # last character always the same
+40.times do
+  pairs, cts = count_pairs pairs, x
+  i += 1
+  if i == 10
+    puts "Part 1: #{cts.values.max - cts.values.min}"
+  end
+  if i == 40
+    puts "Part 2: #{cts.values.max - cts.values.min}"
+  end
+end
