deea39932cbe097e4f60e3b971df12532e520a61
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-17 23:44:59 -0800

    wip

--- 2021/day18.rb
+++ 2021/day18.rb
@@ -1,64 +1,78 @@
-require 'strscan'
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day18test.in").split "\n"
-# input = File.readlines("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day18.in")
-p input
-
-class SF
-  attr_accessor :nested_level
-
-  def initialize val
-    @val = val
-    @level = init_level val
+# input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day18.in").split "\n"
+#
+class Node
+  def initialize
   end
 
-  def init_level val
-    if val.class == Integer
-      0
-    else
-      1 + [init_level(val.first), init_level(val.last)].max
+end
+
+def leaf? n
+  n.class == Integer
+end
+
+def pair? s
+  s.class == Array &&
+    s.first.class == Integer &&
+    s.last.class == Integer
+end
+
+# node must be a pair
+def explode node
+  return if leaf? node
+
+  # traverse tree in-order to find the leaf nodes at level 4
+  layer = []
+  layer << node
+  left = nil
+  right = nil
+  n = 0
+  levels = { node => n }
+
+  while !layer.empty?
+    n += 1
+    next_layer = []
+    layer.each do |node|
+      if n == 5 && pair?(node)
+        p "FOUND IT: #{node}"
+        p [left, right]
+        # do the thing
+      end
+      if !leaf? node
+        x = node.first
+        y = node.last
+        if leaf? x
+          left = [x, n]
+        end
+        if leaf? y
+          right = [y, n]
+        end
+        next_layer << x
+        next_layer << y
+        levels[node] = n
+      end
+      layer = next_layer
     end
+    p "LEVEL: #{n}"
+    puts
   end
+end
 
-  def pair?
-    val.class == Array
-  end
+def add s
+  [val, s]
+end
 
-  def num?
-    val.class == Integer
-  end
+def reduce n
+  actions = []
+  # s = n.to_s
+end
 
-  def magnitude s
-    if s.num?
-      s.val
-    else
-      x = s.val.first
-      y = s.val.last
-      3*(magnitude x) + 2*(magnitude y)
-    end
-  end
-
-  def explode?
-    if nested_level == 5
-      # find leftmost pair to explode
-    end
-  end
-
-  def add s
-    SF.new [val, s]
-  end
-
-  def reduce n
-    actions = []
-    # s = n.to_s
-  end
-
-  def split v
-    n = v/2
-    if v.odd?
-      [n, n+1]
-    else
-      [n, n]
-    end
+def split v
+  n = v/2
+  if v.odd?
+    [n, n+1]
+  else
+    [n, n]
   end
 end
 
@@ -84,13 +98,13 @@ def nested_level val
   end
 end
 
-def explode n
-  # find leftmost pair to explode
-  # x, y = find_pair n
-  s = n.to_s
-  /(\[.*\[.*\[.*\[*)\[(?<x>\d+),(?<y>\d+)\](\].*\].*\].*\]*)/ =~ s
-  x, y = n
-end
+# def explode n
+#   # find leftmost pair to explode
+#   # x, y = find_pair n
+#   s = n.to_s
+#   /(\[.*\[.*\[.*\[*)\[(?<x>\d+),(?<y>\d+)\](\].*\].*\].*\]*)/ =~ s
+#   x, y = n
+# end
 
 def split_num v
   n = v/2
@@ -101,22 +115,13 @@ def split_num v
   end
 end
 
-def split s
-  x, y = s
-end
-
-
 s = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
-p "Part 1: #{magnitude s}"
+# p "Part 1: #{magnitude s}"
 
+# s = [[[[[9,8],1],2],3],4]
+# explode s
+s = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
+explode s
 
-s = [[[[[9,8],1],2],3],4]
-p nested_level s
-s = [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-p nested_level s
-s = [[6,[5,[4,[3,2]]]],1]
-p nested_level s
-
-# p explode s
-s = input.first
-puts s
+# s = [[6,[5,[4,[3,2]]]],1]
+# explode s
