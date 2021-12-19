32e9e20c44f1bc3b015a60105ef7fa56dca98a37
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-18 22:05:16 -0800

    wip

--- 2021/day18.rb
+++ 2021/day18.rb
@@ -1,85 +1,84 @@
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day18test.in").split "\n"
 # input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day18.in").split "\n"
-#
+
 class Node
-  def initialize
+  attr_accessor :left, :right, :val, :parent
+
+  def initialize  val # parent=nil
+    @val = val
+    # @parent = parent
+  end
+
+  def leaf?
+    val.class == Integer
+  end
+
+  def pair?
+    val.class == Array &&
+      left.leaf? &&
+      right.leaf?
+  end
+
+  def nested_level
+    if val.class == Integer
+      0
+    else
+      1 + [left.nested_level, right.nested_level].max
+    end
+  end
+
+  def explodable?
+    nested_level == 5
+  end
+
+  # this is to find left and right vals for explode
+  def inorder res, n
+    n += 1
+    if self.pair? && n == 5
+      # p "FOUND IT: #{val}"
+      res << val
+    elsif self.leaf?
+      res << val
+    else
+      left.inorder res, n
+      right.inorder res, n
+    end
+    res
+  end
+
+  def magnitude
+    if leaf?
+      val
+    else
+      3*(left.magnitude) + 2*(right.magnitude)
+    end
   end
 
 end
 
-def leaf? n
-  n.class == Integer
-end
+# build tree from given snailfish string
+def build_tree v
+  root = Node.new v
+  return root if root.leaf?
+  left_v = v.first
+  right_v = v.last
+  root.left = build_tree left_v
+  root.right = build_tree right_v
 
-def pair? s
-  s.class == Array &&
-    s.first.class == Integer &&
-    s.last.class == Integer
+  root
 end
-
-def inorder node, n, res
-  n+=1
-  if (pair? node) && n == 5
-    p "FOUND IT: #{node}"
-    res << node
-  elsif leaf? node
-    res << node
-  else
-    inorder(node.first, n, res)
-    inorder(node.last, n, res)
-  end
-  res
-end
-
 
 # node must be a pair
 def explode node
   return if leaf? node
-
   # traverse tree in-order to find the leaf nodes at level 4
-  layer = []
-  layer << node
-  left = nil
-  right = nil
-  n = 0
-  levels = { node => n }
-
-  while !layer.empty?
-    n += 1
-    next_layer = []
-    layer.each do |node|
-      if n == 5 && pair?(node)
-        p "FOUND IT: #{node}"
-        p [left, right]
-        # do the thing
-      end
-      if !leaf? node
-        x = node.first
-        y = node.last
-        if leaf? x
-          left = [x, n]
-        end
-        if leaf? y
-          right = [y, n]
-        end
-        next_layer << x
-        next_layer << y
-        levels[node] = n
-      end
-      layer = next_layer
-    end
-    p "LEVEL: #{n}"
-    puts
-  end
 end
 
-def add s
+def add val, s
   [val, s]
 end
 
 def reduce n
-  actions = []
-  # s = n.to_s
 end
 
 def split v
@@ -91,53 +90,15 @@ def split v
   end
 end
 
-def magnitude s
-  if s.class == Integer
-    s
-  else
-    x = s.first
-    y = s.last
-    3*(magnitude x) + 2*(magnitude y)
-  end
-end
-
-def add val, s
-  [val, s]
-end
-
-def nested_level val
-  if val.class == Integer
-    0
-  else
-    1 + [nested_level(val.first), nested_level(val.last)].max
-  end
-end
-
-# def explode n
-#   # find leftmost pair to explode
-#   # x, y = find_pair n
-#   s = n.to_s
-#   /(\[.*\[.*\[.*\[*)\[(?<x>\d+),(?<y>\d+)\](\].*\].*\].*\]*)/ =~ s
-#   x, y = n
-# end
-
-def split_num v
-  n = v/2
-  if v.odd?
-    [n, n+1]
-  else
-    [n, n]
-  end
-end
-
-s = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
-# p "Part 1: #{magnitude s}"
-
 s = [[[[[9,8],1],2],3],4]
-# explode s
-p inorder s, 0, []
-# s = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
-# p inorder s, 0, []
-
+s = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
 s = [[6,[5,[4,[3,2]]]],1]
-p inorder s, 0, []
+# s = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
+
+root = build_tree(s)
+p root.inorder [], 0
+p root.nested_level
+# explode s
+
+p "Part 1: #{root.magnitude}"
+
