3a0b52cd322f319c7e5fddc5d014fc04d80fa4d4
Hsing-Hui Hsu <hhhsu1@gmail.com>
2021-12-19 19:42:58 -0800

    wipp

--- 2021/day18.rb
+++ 2021/day18.rb
@@ -31,20 +31,49 @@ class Node
     nested_level == 5
   end
 
-  # this is to find left and right vals for explode
-  def inorder res, n
+  # traverse tree in order - returns sequence of leaf values
+  # this is to find left and right vals for explode addition
+  # def inorder res, n
+  #   n += 1
+  #   if pair? && n == 5
+  #     # p "FOUND IT: #{val}"
+  #     res << val
+  #   elsif leaf?
+  #     res << val
+  #   else
+  #     left.inorder res, n
+  #     right.inorder res, n
+  #   end
+  #   res
+  # end
+  def inorder seq, n
     n += 1
-    if self.pair? && n == 5
+    p self.val
+    if pair? && n == 5
       # p "FOUND IT: #{val}"
-      res << val
-    elsif self.leaf?
-      res << val
+      seq << val
+    elsif leaf?
+      seq << val
     else
-      left.inorder res, n
-      right.inorder res, n
+      left.inorder seq, n
+      right.inorder seq, n
     end
-    res
+    seq
   end
+
+  # def traverse_split
+  #   # p self.val
+  #   if leaf? && val > 9
+  #     self.val = split_val
+  #     # insert
+  #     return
+  #   elsif leaf?
+  #     return
+  #   else
+  #     left.traverse_split
+  #     right.traverse_split
+  #   end
+  # end
 
   def magnitude
     if leaf?
@@ -52,6 +81,43 @@ class Node
     else
       3*(left.magnitude) + 2*(right.magnitude)
     end
+  end
+
+  def add v
+    Node.new [val, v]
+  end
+
+  # BFS to replace node
+  def split
+    queue = []
+    queue << self
+    while !queue.empty?
+      curr = queue.shift
+      if curr.leaf? && curr.val > 9
+        curr.val = curr.split_val
+        break
+      else
+        queue << curr.left
+        queue << curr.right
+      end
+    end
+  end
+
+  def split_val
+    n = val/2
+    if val.odd?
+      [n, n+1]
+    else
+      [n, n]
+    end
+  end
+
+  # node must be a pair
+  def explode node
+    return if leaf?
+    if explodable?
+    end
+    # traverse tree in-order to find the leaf nodes at level 4
   end
 
 end
@@ -68,37 +134,24 @@ def build_tree v
   root
 end
 
-# node must be a pair
-def explode node
-  return if leaf? node
-  # traverse tree in-order to find the leaf nodes at level 4
-end
-
-def add val, s
-  [val, s]
-end
-
 def reduce n
 end
 
-def split v
-  n = v/2
-  if v.odd?
-    [n, n+1]
-  else
-    [n, n]
-  end
-end
-
 s = [[[[[9,8],1],2],3],4]
-s = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
-s = [[6,[5,[4,[3,2]]]],1]
+# s = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
+# s = [[6,[5,[4,[3,2]]]],1]
 # s = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
 
+s = [[[[0,7],4],[15,[0,13]]],[1,1]]
 root = build_tree(s)
-p root.inorder [], 0
 p root.nested_level
+
+puts "BEFORE SPLIT"
+p "SEQ: #{root.inorder [], 0}"
+root.split
+puts "AFTER SPLIT"
+# p root
+p "SEQ: #{root.inorder [], 0}"
 # explode s
 
-p "Part 1: #{root.magnitude}"
-
+# p "Part 1: #{root.magnitude}"
