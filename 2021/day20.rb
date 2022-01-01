f9a79fc924787116c97afe9a14750bbd7d2fd985
Hsing-Hui Hsu <hhhsu1@gmail.com>
2022-01-01 18:54:06 -0500

    day 20 hacky af

--- 2021/day20.rb
+++ 2021/day20.rb
@@ -2,10 +2,9 @@ require 'pp'
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day20test.in").split("\n\n")
 input = File.read("/Users/hhh/JungleGym/advent_of_code/2021/inputs/day20.in").split("\n\n")
 
-# algo, x = input
-# @algo = algo.split("\n").join
+algo, x = input
+@algo = algo.split("\n").join
 
-@algo, x = input
 img = Hash.new
 
 x.split("\n").each_with_index do |row, i|
@@ -17,7 +16,37 @@ x.split("\n").each_with_index do |row, i|
   img
 end
 
-def px_val x, y, img
+def px_val2 x, y, img, on
+  min = img.keys.min
+  max = img.keys.max
+
+  adj = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1], [ 0, 0], [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1],
+  ].map do |(i,j)|
+    [x+i, y+j]
+  end
+
+  b = adj.map do |(dx, dy)|
+    if (min <= dx && dx <= max) && (min <= dy && dy <= max)
+      img[dx][dy]
+    else
+      "#"
+      # if on
+      #   "."
+      # else
+      #   "#"
+      # end
+    end
+  end.map do |c|
+    c == "." ? 0 : 1
+  end.join
+
+  i = b.to_i(2)
+  @algo[i]
+end
+def px_val x, y, img, on
   min = img.keys.min
   max = img.keys.max
 
@@ -34,6 +63,11 @@ def px_val x, y, img
       img[dx][dy]
     else
       "."
+      # if on
+      #   "."
+      # else
+      #   "#"
+      # end
     end
   end.map do |c|
     c == "." ? 0 : 1
@@ -43,7 +77,7 @@ def px_val x, y, img
   @algo[i]
 end
 
-def enhance img
+def enhance img, on
   count = 0
   min = img.keys.min
   max = img.keys.max
@@ -53,7 +87,11 @@ def enhance img
   (min-1..max+1).each do |i|
     new_img[i] = Hash.new
     (min-1..max+1).each do |j|
-      px = px_val i, j, img
+      px = if !on
+      px_val i, j, img, on
+      else
+       px_val2 i, j, img, on
+      end
       count += 1 if px == "#"
       new_img[i][j] = px
     end
@@ -72,12 +110,11 @@ def display img
   end
 end
 
-2.times do
-  img, count = enhance img
-  display img
+25.times do
+  img, count = enhance img, false
+  # display img
   p count
-  puts
+  img, count = enhance img, true
+  # display img
+  p count
 end
-
-# 5860 too high
-# 5658 too low
