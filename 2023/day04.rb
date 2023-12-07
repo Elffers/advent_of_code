65b27082a3e3866b4aa49f9cf13c03adeed2920d
Hsing-Hui Hsu <hsing-hui.hsu@webflow.com>
2023-12-07 02:03:42 -0800

    day 4 pt 2

--- 2023/day04.rb
+++ 2023/day04.rb
@@ -1,14 +1,22 @@
 require 'set'
 
 cards = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day04.in").split("\n")
-cards = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day04_test.in").split("\n")
+# cards = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day04_test.in").split("\n")
 
 sum = 0
 
 CARDS = Hash.new 0
+WINNING = {}
 
 cards.each do |card|
-  x, y = card.split(": ").last.split(" | ")
+  card_id, lists = card.split(": ")
+  /Card\s+(?<id>\d+)/ =~ card_id
+  id = id.to_i
+  WINNING[id] = 0
+  CARDS[id] += 1
+
+  x, y = lists.split(" | ")
+
   winning = Set.new
   x.split.each {|n| winning << n.to_i }
   nums = y.split.map {|n| n.to_i }
@@ -16,6 +24,7 @@ cards.each do |card|
   i = -1
   nums.each do |n|
     if winning.include? n
+      WINNING[id] += 1
       i += 1
     end
   end
@@ -24,5 +33,11 @@ cards.each do |card|
     sum += 2**i
   end
 end
-p sum
-# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+p "Part 1: #{sum}"
+
+WINNING.each_pair do |id, n|
+    (1..n).each do |i|
+      CARDS[id+i] += CARDS[id]
+    end
+end
+p "Part 2: #{CARDS.values.sum}"
