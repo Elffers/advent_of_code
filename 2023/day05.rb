223fe872789be2d8469a023f153d428a4f8f7a0e
Hsing-Hui Hsu <hsing-hui.hsu@webflow.com>
2023-12-09 13:37:51 -0800

    day 5 pt 1

--- 2023/day05.rb
+++ 2023/day05.rb
@@ -1,33 +1,38 @@
 require 'pp'
 maps = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day05.in").split("\n\n")
-# maps = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day05_test.in").split("\n\n")
+maps = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day05_test.in").split("\n\n")
 
 seeds = maps.shift.split(": ").last.split.map { |x| x.to_i }
-p "Seeds: #{seeds}"
+
+def check_lists s, lists
+  lists.each do |list|
+    dest = list[0]
+    src = list[1]
+    len = list[2]
+    if s >= src && s < (src+len)
+      diff = s-src
+      return dest + diff
+    end
+  end
+  nil
+end
 
 # input array, return array
 def src_to_dest data, sources
   lists = data.split("\n")
-  lists.shift # remove header
+  p lists.shift # remove header
   lists = lists.map { |list| list.split.map { |x| x.to_i } }
 
-  mapping = {}
-  lists.each do |list|
-    dest = list.shift
-    src = list.shift
-    len = list.shift
-    i = 0
-    while i <= len
-      mapping[src+i] = dest + i
-      i += 1
-    end
+  out = sources.map do |s|
+    check_lists(s, lists) || s
   end
-  sources.map { |x| mapping[x] || x }
+  out
 end
 
 src = seeds
 while !maps.empty?
+  # p src
   m = maps.shift
   src = src_to_dest m, src
 end
-p src.min
+p "Part 1: #{src.min}"
