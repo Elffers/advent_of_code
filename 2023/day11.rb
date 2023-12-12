ff367ba206a1bc5587348c542b58eb6b693ea7a8
Hsing-Hui Hsu <hsing-hui.hsu@webflow.com>
2023-12-11 23:22:44 -0800

    day 11

--- 2023/day11.rb
+++ 2023/day11.rb
@@ -1,37 +1,40 @@
 input = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day11.in").split("\n")
-# input = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day11_test.in").split("\n")
-expanded = []
-
 
 empty_cols = []
-input.each.with_index do |row, i|
+cols = input.first.size
+i = 0
+
+while i < cols
   if !input.map { |row| row[i] }.include? "#"
     empty_cols << i
   end
+  i += 1
 end
 
-expanded = []
+empty_rows = []
+
+input.each.with_index do |row, i|
+  if !row.include? "#"
+    empty_rows << i
+  end
+end
 
 gals = []
 input.each.with_index do |row, i|
-  expanded_row = ""
-  row.chars.each.with_index do |char, j|
-      expanded_row << char
-    if empty_cols.include? j
-      expanded_row << char
-    end
-  end
-  expanded << expanded_row
-
-  if !expanded_row.include? "#"
-    expanded << expanded_row
-  end
-end
-
-expanded.each.with_index do |row, i|
   row.chars.each.with_index do |char, j|
     if char == "#"
-      gals << [i, j]
+      p = i
+      q = j
+
+      empty_rows.each do |x|
+        p += 999_999 if x < i
+      end
+
+      empty_cols.each do |y|
+        q += 999_999 if y < j
+      end
+
+      gals << [p, q]
     end
   end
 end
@@ -44,6 +47,4 @@ gals.each do |(x, y)|
   end
 end
 p sum/2
-
-
 
