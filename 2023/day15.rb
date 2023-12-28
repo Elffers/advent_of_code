31a5eb97d3245aa10e1f7e115f6cf2f21d88e277
Hsing-Hui Hsu <hsing-hui.hsu@webflow.com>
2023-12-27 23:57:20 -0800

    day15

--- 2023/day15.rb
+++ 2023/day15.rb
@@ -1,12 +1,11 @@
 input = File.read("/Users/hhhsu/sandbox/advent_of_code/2023/inputs/day15.in").chomp.split(",")
-p input
 
 # Determine the ASCII code for the current character of the string.
 # Increase the current value by the ASCII code you just determined.
 # Set the current value to itself multiplied by 17.
 # Set the current value to the remainder of dividing itself by 256.
 
-def foo string
+def find_hash string
   curr = 0
   string.chars.each do |char|
     curr += char.ord
@@ -16,11 +15,38 @@ def foo string
   curr
 end
 
+def find_hashmap str
+  if str.chars.last == "-"
+    str = str.chop
+  end
+  if str.include? "="
+    str, box = str.split "="
+  end
+  [str, find_hash(str), box]
+end
+
 sum = 0
 
+hashmap = Hash.new { |h, k| h[k] = {} }
+
 input.each do |str|
-  x = foo str
-  p x
+  x = find_hash str
+  label, box, val = find_hashmap str
+  if !val.nil?
+    hashmap[box][label] = val
+  else
+    hashmap[box].delete label
+  end
   sum += x
 end
-p sum
+
+p "Part 1: #{sum}"
+
+sum = 0
+hashmap.each do |box, lenses|
+  lenses.each.with_index do |lens, i|
+    pwr = (box+1) * (i+1) * lens.last.to_i
+    sum += pwr
+  end
+end
+p "part 2: #{sum}"
